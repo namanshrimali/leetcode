a2ab5162823c082953afc9cba6ac7a9426505433
Naman Shrimali <namanshrimali@gmail.com>
2021-12-27 22:03:29 -0500

    Update valid-palindrome-ii.py

--- valid-palindrome-ii/valid-palindrome-ii.py
+++ valid-palindrome-ii/valid-palindrome-ii.py
@@ -5,7 +5,7 @@ class Solution:
         l, r = 0, len(s)-1
         while(l<r):
             if s[l]!=s[r]:
-                return is_palindrome(s[0:l]+s[l+1:len(s)]) or is_palindrome(s[0:r]+s[r+1:len(s)]) 
+                return is_palindrome(s[l+1:r+1]) or is_palindrome(s[l:r]) 
             l+=1
             r-=1
         return True
