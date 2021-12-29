8069299046de30005d122910c71c174bfe8bb69b
Naman Shrimali <namanshrimali@gmail.com>
2021-12-28 22:51:42 -0500

    56 ms, faster than 96.40% 17.1 MB, less than 85.12%

--- moving-average-from-data-stream/moving-average-from-data-stream.py
+++ moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -1,12 +1,17 @@
 class MovingAverage:
 
     def __init__(self, size: int):
-        self.queue = collections.deque(maxlen=size)
+        self.queue = [0 for i in range(size)]
+        self.size = size
+        self.count = -1
+        self.window_sum = 0
 
     def next(self, val: int) -> float:
-        self.queue.append(val)
-        return sum(self.queue)/len(self.queue)
-        
+        self.count+=1
+        tail = self.count%self.size
+        self.window_sum+= val - self.queue[tail]
+        self.queue[tail] =  val
+        return self.window_sum/min(self.count+1, len(self.queue))
 
 
 # Your MovingAverage object will be instantiated and called as such:
