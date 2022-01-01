a53875661d55944705def96f23caf88062dd41ab
Naman Shrimali <namanshrimali@gmail.com>
2021-12-31 19:05:38 -0500

    Time: 480 ms (83.60%), Space: 14.6 MB (81.33%) - LeetHub

--- island-perimeter/island-perimeter.py
+++ island-perimeter/island-perimeter.py
@@ -1,23 +1,19 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        visited = set()
-        def dfs(row, col):
-            nonlocal diameter
-            if (row, col) in visited:
-                return
-            if not(-1<row<len(grid)) or not(-1<col<len(grid[0])) or grid[row][col] != 1:
-                diameter+=1
-                return
-            visited.add((row, col))
-            for x_dir, y_dir in directions:
-                dfs(row+x_dir, col+y_dir)
-                
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-        diameter = 0
+        rows = len(grid)
+        cols = len(grid[0])
         
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if (i, j) not in visited and grid[i][j] == 1:
-                    dfs(i, j)
-        return diameter
-    
+        result = 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    result += 4
+                    
+                    if r > 0 and grid[r-1][c] == 1:
+                        result -= 2
+                        
+                    if c > 0 and grid[r][c-1] == 1:
+                        result -= 2
+        
+        return result
