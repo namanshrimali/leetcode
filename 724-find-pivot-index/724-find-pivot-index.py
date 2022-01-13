d0eb4d4c74aa77f1cc2eb98bf8b25e1d0f0ed236
Naman Shrimali <namanshrimali@gmail.com>
2022-01-13 16:56:58 -0500

    Time: 198 ms (39.35%), Space: 15.5 MB (41.28%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,13 +1,9 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        sum_arr = [0]*len(nums)
-        total = 0
+        total = sum(nums)
+        left = 0
         for i in range(len(nums)):
-            sum_arr[i] = total
-            total+=nums[i]
-        total, sol = 0, -1
-        for i in range(len(nums)-1, -1, -1):
-            if sum_arr[i] == total:
-                sol = i
-            total+=nums[i]
-        return sol
+            if left == total - left - nums[i]:
+                return i
+            left+=nums[i]
+        return -1
