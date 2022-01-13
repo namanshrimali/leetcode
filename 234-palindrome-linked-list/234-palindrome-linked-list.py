5747440b9fcfc9a9aff5779961437700857925fc
Naman Shrimali <namanshrimali@gmail.com>
2022-01-13 16:29:52 -0500

    Time: 1131 ms (18.65%), Space: 39.4 MB (64.46%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,9 +5,32 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        num = []
+        def get_end_of_first_half(start):
+            hare, tortoise = start, start
+            while(hare.next and hare.next.next):
+                hare = hare.next.next
+                tortoise = tortoise.next
+            return tortoise
         
-        while(head):
-            num.append(head.val)
-            head = head.next
-        return num == num[::-1]
+        def reverse_list(node):
+            prev = None
+            curr = node
+            
+            while(curr):
+                next_node = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_node
+            return prev
+        
+        first_half = get_end_of_first_half(head)
+        second_half_start = reverse_list(first_half.next)
+        
+        first, last = head, second_half_start
+        sol = True
+        while(first and last):
+            if first.val != last.val:
+                sol = False
+            first = first.next
+            last = last.next
+        return sol
