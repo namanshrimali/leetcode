d5dee03deb463399d93ad72c1d71782f770a2251
Naman Shrimali <namanshrimali@gmail.com>
2022-01-24 08:15:45 -0500

    Time: 144 ms (41.45%), Space: 18.8 MB (12.76%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -6,7 +6,6 @@ class Solution:
         for start, end in intervals[1:]:
             last_start, last_end = sol[-1]
             if start <= last_end:
-                sol[-1][0] = min(last_start, start)
                 sol[-1][1] = max(last_end, end)
             else:
                 sol.append([start, end])
