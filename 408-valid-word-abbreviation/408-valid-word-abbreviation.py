06e1521cd4ee207c5bce4bcfccdf18b635e0b673
Naman Shrimali <namanshrimali@gmail.com>
2022-01-24 09:03:01 -0500

    Time: 62 ms (6.77%), Space: 14.1 MB (83.69%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.py
@@ -1,23 +1,22 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        i, j = 0, 0
-        while(i<len(abbr)):
-            if abbr[i].isdigit():
-                # returning 0 as starts with 0
-                if abbr[i] == '0':
+        idx, abbr_idx = 0, 0
+        L, L_ABBR = len(word), len(abbr)
+        while(idx<L and abbr_idx<L_ABBR):
+            if word[idx]!=abbr[abbr_idx]:
+                if abbr[abbr_idx].isdigit():
+                    digit = int(abbr[abbr_idx])
+                    if digit == 0:
+                        return False
+                    while(abbr_idx+1 < L_ABBR and abbr[abbr_idx+1].isdigit()):
+                        abbr_idx+=1
+                        digit = digit*10+int(abbr[abbr_idx])
+                    if idx+digit-1 >= L:
+                        return False
+                    idx+=digit-1
+                else:
                     return False
-                # calculating num
-                num = int(abbr[i])
-                while(i+1<len(abbr) and abbr[i+1].isdigit()):
-                    num = 10*num+int(abbr[i+1])
-                    i+=1
-                # if num exceeds size of word
-                if j+num-1 >= len(word):
-                    return False
-                j+=num-1
-            else:
-                if j >= len(word) or abbr[i] != word[j]:
-                    return False
-            i+=1
-            j+=1
-        return True if j == len(word) else False
+            idx+=1
+            abbr_idx+=1
+        return True if abbr_idx == L_ABBR and idx==L else False
+            
