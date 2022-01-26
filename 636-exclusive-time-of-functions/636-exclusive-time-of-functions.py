73ad90290f4c22c0abe3e5ac3099090c6edc7342
Naman Shrimali <namanshrimali@gmail.com>
2022-01-26 17:03:49 -0500

    Time: 76 ms (64.42%), Space: 14.4 MB (57.97%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
@@ -1,20 +1,18 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         sol = [0]*n
-        execution_stack = []
+        stack, prev_time = [], 0
         for log in logs:
-            func, phase, time = log.split(":")
+            func, process, time = log.split(":")
             func, time = int(func), int(time)
-            if phase == 'start':
-                if execution_stack:
-                    prev_func, prev_time = execution_stack[-1]
-                    sol[prev_func]+=time - prev_time
-                execution_stack.append([func, time])
-                
-            else:
-                _, prev_time = execution_stack.pop()
-                sol[func]+= time - prev_time + 1
-                if execution_stack:
-                    execution_stack[-1][1]= time+1
-        return sol
+            
+            if process == 'start':
+                if stack:
+                    sol[stack[-1]]+=time-prev_time
+                stack.append(func)
+                prev_time = time
                     
+            else:
+                sol[stack.pop()]+= time-prev_time+1
+                prev_time = time+1
+        return sol
