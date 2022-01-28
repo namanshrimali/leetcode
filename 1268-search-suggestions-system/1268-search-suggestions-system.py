cff13c01d0e7ba3058de54e2c6eefab938888838
Naman Shrimali <namanshrimali@gmail.com>
2022-01-28 15:15:50 -0500

    Time: 111 ms (75.00%), Space: 17 MB (90.56%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,17 +1,24 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products = collections.deque(products)
+        products.sort()
+        def binary_search(prefix, left, right = len(products)-1):
+            prefix_len = len(prefix)
+            while left < right:
+                mid = (left+right)//2
+                if products[mid][:prefix_len] < prefix:
+                    left = mid+1
+                else:
+                    right = mid
+            return right
+        
+        start = 0
         sol = []
         for i in range(len(searchWord)):
-            heap, L = [], len(products)
-            for _ in range(L):
-                product = products.popleft()
-                if product[:i+1] == searchWord[:i+1]:
-                    # heapq.heappush(heap, product)
-                    # if len(heap) > 3:
-                    #     heap.pop()
-                    heap.append(product)
-                    products.append(product)
-            heap.sort()
-            sol.append(heap[:3])
+            prefix = searchWord[:i+1]
+            start = binary_search(prefix, start)
+            sub_sol = []
+            for j in range(3):
+                if start+j < len(products) and products[start+j][:i+1] == prefix:
+                    sub_sol.append(products[start+j])
+            sol.append(sub_sol)
         return sol
