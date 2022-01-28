269cd5fada4e1c4743fe836a170ba13065cf98fd
Naman Shrimali <namanshrimali@gmail.com>
2022-01-28 09:16:45 -0500

    Time: 272 ms (35.17%), Space: 14.9 MB (77.20%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -4,9 +4,9 @@ class Solution:
         for letter in s:
             hash_map[letter] = hash_map.get(letter, 0) + 1
         sol, found_freq = 0, set()
-        heap = [(hash_map[key], key) for key in hash_map]
+        heap = [(hash_map[key]) for key in hash_map]
         # heapq.heapify(heap)
-        for freq, ele in heap:
+        for freq in heap:
             if freq in found_freq:
                 exceed = 0
                 while freq >0 and freq in found_freq:
