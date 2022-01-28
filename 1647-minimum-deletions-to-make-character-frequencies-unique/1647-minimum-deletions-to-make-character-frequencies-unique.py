ac1a408c896f7b40fc8f29b9abd3268745335002
Naman Shrimali <namanshrimali@gmail.com>
2022-01-28 09:13:50 -0500

    Time: 454 ms (8.56%), Space: 14.9 MB (45.19%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        hash_map = {}
+        for letter in s:
+            hash_map[letter] = hash_map.get(letter, 0) + 1
+        sol, found_freq = 0, set()
+        heap = [(hash_map[key], key) for key in hash_map]
+        heapq.heapify(heap)
+        while heap:
+            freq, ele = heapq.heappop(heap)
+            if freq in found_freq:
+                exceed = 0
+                while freq >0 and freq in found_freq:
+                    freq-=1
+                    exceed+=1
+                sol += exceed
+            found_freq.add(freq)
+        return sol
