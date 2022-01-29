f13075d117e57b8d91deabe71b3088a657193895
Naman Shrimali <namanshrimali@gmail.com>
2022-01-29 16:03:28 -0500

    Time: 391 ms (43.51%), Space: 15 MB (56.21%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -1,14 +1,16 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         L = len(s)
-        prefix = [0]*(L+1)
+        flips, ones = 0, 0
+        # aim to always flip zeros
         for i in range(L):
-            prefix[i+1] = prefix[i]+int(s[i])
-        sol = float('inf')
-        for i in range(L+1):
-            ones_till_i, ones_after_i = prefix[i], prefix[L]-prefix[i]
-            zeros_after_i = L-i-ones_after_i
-            # need to flip ones before i and zeros after i to make string monotonically increasing, min is kept in sol
-            sol = min(sol, ones_till_i+zeros_after_i)
-        return sol
-            
+            if s[i] == '1':
+                ones+=1
+            else:
+                flips+=1    
+            flips = min(ones, flips)
+        return flips
+    
+    
+    
+    
