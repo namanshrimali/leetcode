4015baa01e7a425bd82aa7b3214edab20b078e9c
Naman Shrimali <namanshrimali@gmail.com>
2022-01-30 16:13:33 -0500

    Time: 708 ms (61.79%), Space: 18.2 MB (10.27%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,19 +1,20 @@
 class Solution:
-	def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-		seats = collections.defaultdict(set)
-
-		for i,j in reservedSeats:
-			if j in [2,3,4,5]:
-				seats[i].add(0)
-			if j in [4,5,6,7]:
-				seats[i].add(1)
-			if j in [6,7,8,9]:
-				seats[i].add(2)
-		res = 2*n
-		for i in seats:
-			if len(seats[i]) == 3:
-				res -= 2
-			else:
-				res -= 1
-
-		return res
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        acceptable_groups = [[2,3,4,5], [4,5,6,7], [6,7,8,9]]
+        seat_map = {}
+        for row, seat in reservedSeats:
+            blocked_groups = set()
+            for i in range(3):
+                if seat in acceptable_groups[i]:
+                    blocked_groups.add(i)
+            if blocked_groups:
+                seat_map[row] = seat_map.get(row, set()).union(blocked_groups)
+        
+        total_groups = 2*n
+        for row in seat_map:
+            if len(seat_map[row]) == 3:
+                total_groups-=2
+            else:
+                total_groups-=1
+                
+        return total_groups
