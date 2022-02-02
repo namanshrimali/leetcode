7d8425156187efbe7978eb0629e1f98c990588dd
Naman Shrimali <namanshrimali@gmail.com>
2022-02-01 23:58:53 -0500

    Time: 32 ms (79.03%), Space: 13.9 MB (97.13%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.py
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -1,12 +1,8 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         hash_map = {}
-        for char in s:
-            hash_map[char] = hash_map.get(char, 0)+1
-        ans = []
-        for o in order:
-            ans.append(o*hash_map.get(o, 0))
-            hash_map[o] = 0
-        for char in hash_map:
-            ans.append(char*hash_map[char])
-        return ''.join(ans)
+        for i in range(len(order)):
+            hash_map[order[i]] = i+1
+        s = list(s)
+        s.sort(key= lambda a:hash_map.get(a, 27))
+        return ''.join(s)
