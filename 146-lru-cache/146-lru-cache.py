5bd929598df422749a3e78859f43f74fbfed19ef
Naman Shrimali <namanshrimali@gmail.com>
2022-02-02 00:44:28 -0500

    Time: 1804 ms (12.70%), Space: 76.1 MB (49.02%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,57 +1,49 @@
 class Node:
-    def __init__(self, key, val, prev=None, next=None):
+    def __init__(self, key=None, val=None, prev=None, next=None):
         self.key = key
         self.val = val
         self.prev = prev
         self.next = next
-        
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.node_map = {}
-        self.head = Node(None, None)
-        self.tail = Node(None, None)
+        self.head, self.tail = Node(), Node()
         self.head.next = self.tail
         self.tail.prev = self.head
+        self.lru = {}
         
-    def delete_node(self, node):
+    def add_to_top(self, node):
+        node.next = self.head.next
+        node.next.prev = node
+        node.prev = self.head
+        self.head.next = node
+    
+    def remove_node(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
-    
-    def add_node(self, node):
-        node.next = self.tail
-        self.tail.prev.next = node
-        node.prev = self.tail.prev
-        self.tail.prev = node
-        
-        
-    def move_to_tail(self, node):
-        self.delete_node(node)
-        self.add_node(node)
-        
+        return node
         
     def get(self, key: int) -> int:
-        node = self.node_map.get(key, None)
-        if node:
-            self.move_to_tail(node)
+        if key in self.lru:
+            node = self.lru[key]
+            self.add_to_top(self.remove_node(node))
             return node.val
-        return -1
-        
+        else:
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        node = self.node_map.get(key)
-        if node is None:
-            node = Node(key, value)
-            self.node_map[key] = node
-            self.add_node(node)
-
-            if len(self.node_map)>self.capacity:
-                del self.node_map[self.head.next.key]
-                self.delete_node(self.head.next)
-        else:
+        if key in self.lru:
+            node = self.lru[key]
             node.val = value
-            self.move_to_tail(node)
-
+            self.add_to_top(self.remove_node(node))
+        else:
+            node = Node(key, value)
+            self.lru[key] = node
+            self.add_to_top(node)
+            if len(self.lru)>self.capacity:
+                del_node = self.remove_node(self.tail.prev)
+                del self.lru[del_node.key]
+                
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
