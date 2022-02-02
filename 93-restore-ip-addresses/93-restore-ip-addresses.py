63b30531a138885aaad1f182f3595bff6e8eb106
Naman Shrimali <namanshrimali@gmail.com>
2022-02-02 09:37:46 -0500

    Time: 107 ms (5.32%), Space: 13.9 MB (98.84%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -1,30 +1,27 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        def valid(segment):
+            if segment[0] != '0':
+                return int(segment)<=255
+            else:
+                return len(segment)==1
         def form_ip(ip_arr = [], idx=0, part_remaining=4):
-            nonlocal sol            
+            nonlocal sol        
             if idx == len(s):
                 if part_remaining == 0:
                     sol.append(''.join(ip_arr))
                 return
             
-            if s[idx]=='0':
+            i = idx+1
+            segment = s[idx:i]
+            while i<=len(s) and valid(segment):
                 if part_remaining>1:
-                    updated_ip_arr = ip_arr+[s[idx], '.']
+                    updated_ip_arr = ip_arr+[segment, '.']
                 else:
-                    updated_ip_arr = ip_arr+[s[idx]]
-                form_ip(updated_ip_arr, idx+1, part_remaining-1)
-                
-            else:
-                i = idx+1
-                while i<=len(s) and i-idx<=3 and int(s[idx:i])<=255:
-                    if part_remaining>1:
-                        updated_ip_arr = ip_arr+[s[idx:i], '.']
-                    else:
-                        updated_ip_arr = ip_arr+[s[idx:i]]
-                    
-                    form_ip(updated_ip_arr, i, part_remaining-1)
-                    
-                    i+=1
+                    updated_ip_arr = ip_arr+[segment]
+                form_ip(updated_ip_arr, i, part_remaining-1)  
+                i+=1
+                segment = s[idx:i]
         sol = []
         form_ip()
         return sol
