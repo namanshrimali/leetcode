df4f3f5f1388d2f056710de71fd438ad68df7cd4
Naman Shrimali <namanshrimali@gmail.com>
2022-02-03 12:38:05 -0500

    Time: 1494 ms (9.76%), Space: 20.4 MB (23.18%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,32 +1,14 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def distance_from_origin(x, y):
+        def distance_from_origin(point):
+            x, y = point[0], point[1]
             return math.sqrt(x**2 + y**2)
-        
-        def partition(start, end):
-            pivot = random.randint(start, end)
-            points[pivot], points[end] = points[end], points[pivot]
-            pivot = end
-            pivot_distance = distance_from_origin(points[pivot][0], points[pivot][1])
-            i = start-1
-            while(start<end):
-                if distance_from_origin(points[start][0], points[start][1]) <= pivot_distance:
-                    i+=1
-                    points[start], points[i] = points[i], points[start]
-                start+=1
-            i+=1
-            points[pivot], points[i] = points[i], points[pivot]
-            return i
-        
-        left, right = 0, len(points)-1
-        q = right+1
-        while q!=k:
-            q = partition(left, right)
-            if q > k:
-                right = q-1
-            else:
-                left = q+1
-        return points[:k]
+        heap = []
+        for point in points:
+            heapq.heappush(heap, (-distance_from_origin(point), point[0], point[1]))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return [[x, y] for dist, x, y in heap]
         
                 
                 
