b0e35ff5918674d59d2d077aadea00dd33e9cf1e
Naman Shrimali <namanshrimali@gmail.com>
2022-02-04 12:24:38 -0500

    Time: 146 ms (11.52%), Space: 21.5 MB (83.88%) - LeetHub

---
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.py
@@ -0,0 +1,46 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def inorderSuccessor(self, node: 'Node') -> 'Optional[Node]':
+        # when node is left leaf -> parent node
+        # when node is right leaf -> leftmost child of grand parent's right child else None
+        # when node is parent with right child -> leftmost of right child
+        # when node has no right child -> leftmost child of parent's right node
+        def is_leaf(node):
+            return True if node.left == node.right == None and node.parent != None else False
+        def get_leftmost(node):
+            if node is None:
+                return
+            while node.left:
+                node = node.left
+            return node
+        def get_next_largest(node):
+            prev, parent = node, node.parent
+            while parent and parent.right == prev:
+                prev = parent
+                parent = parent.parent
+            return parent
+            
+        
+        if is_leaf(node):
+            parent_right = node.parent.right
+            if node!=parent_right:
+                return node.parent
+            else:
+                return get_next_largest(node)
+        else:
+            if node.right:
+                return get_leftmost(node.right)
+            else:
+                if node.parent:
+                    return get_next_largest(node)
+        return None
+        
