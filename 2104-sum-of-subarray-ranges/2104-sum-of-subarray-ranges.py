11e7a6cf8c5b2a2e6c4dc400c0540d9472e06d63
Naman Shrimali <namanshrimali@gmail.com>
2022-02-06 14:25:10 -0500

    Time: 92 ms (93.82%), Space: 14.1 MB (95.26%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -4,6 +4,18 @@ class Solution:
         min_stack = []
         
         min_nums = [float('-inf')]+nums+[float('-inf')]
+        
+        # in the below for loop, we calculate the total number of sub arrays where a number is minimum
+        # When we encounter a number that is less than the last number,  we pop out one by one all the numbers in 
+        # the min stack which are necessarily greater than the current number. We then calculate the number of 
+        # subarrays where those popped number are minimum. Since in min stack, all numbers are necessarily in 
+        # increasing order, to calulate number of subarrays on left where popped number is minimum, we subtract the 
+        # popped number index with the index on top of min stack. Similarly, we calculate number of subarrays on the 
+        # right of popped number in which it is minimum. Since current number is minimum, subarray in the right would 
+        # be curr idx - popped idx
+        # exactly same logic is used for calculating number of subarrays where number is maximum
+        
+        
         for i in range(len(min_nums)):
             num = min_nums[i]
             while min_stack and min_nums[min_stack[-1]] > num:
