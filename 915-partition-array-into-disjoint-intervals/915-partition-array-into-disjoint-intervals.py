2825960ed60ba5d4c18545052c08339196254af3
Naman Shrimali <namanshrimali@gmail.com>
2022-02-09 09:27:27 -0500

    Time: 1740 ms (32.70%), Space: 27.2 MB (79.73%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.py
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.py
@@ -1,15 +1,16 @@
 class Solution:
     def partitionDisjoint(self, nums: List[int]) -> int:
         L = len(nums)
-        left_max, right_min = [0]*L, [0]*L
-        left_max[0], right_min[L-1] = nums[0], nums[L-1]
-        sol_idx = 0
+        right_min = [0]*L
+        right_min[L-1] = nums[L-1]
+        
         for i in range(L-2, -1, -1):
             right_min[i] = min(right_min[i+1], nums[i])
-            
+        
+        curr_left_max = nums[0]
         for i in range(1, L):
-            left_max[i] = max(left_max[i-1], nums[i])
-            if right_min[i] >= left_max[i-1]:
+            if right_min[i] >= curr_left_max:
                 return i
+            curr_left_max = max(curr_left_max, nums[i])
         return -1
         
