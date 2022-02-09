1c7e65f6f7d064dd94df70d6d892bad6b89a9302
Naman Shrimali <namanshrimali@gmail.com>
2022-02-09 09:24:02 -0500

    Time: 1204 ms (72.16%), Space: 27.1 MB (84.32%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.py
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.py
@@ -4,11 +4,12 @@ class Solution:
         left_max, right_min = [0]*L, [0]*L
         left_max[0], right_min[L-1] = nums[0], nums[L-1]
         sol_idx = 0
+        for i in range(L-2, -1, -1):
+            right_min[i] = min(right_min[i+1], nums[i])
+            
         for i in range(1, L):
             left_max[i] = max(left_max[i-1], nums[i])
-        for i in range(L-2, -1, -1):
-            right_min[i] = min(right_min[i+1], nums[i+1])
-            if right_min[i] >= left_max[i]:
-                sol_idx = i
-        return sol_idx+1
+            if right_min[i] >= left_max[i-1]:
+                return i
+        return -1
         
