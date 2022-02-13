2ea87b405f6516b6047758211aa0cab3cd36ce3d
Naman Shrimali <namanshrimali@gmail.com>
2022-02-13 10:33:59 -0500

    Time: 1371 ms (17.88%), Space: 20.3 MB (33.50%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,15 +1,33 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def distance_from_origin(point):
-            x, y = point[0], point[1]
-            return math.sqrt(x**2 + y**2)
-        heap = []
-        for point in points:
-            heapq.heappush(heap, (-distance_from_origin(point), point[0], point[1]))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return [[x, y] for dist, x, y in heap]
+        def euc_dist(point):
+            return point[0]**2+point[1]**2
         
-                
+        def partition(left, right):
+            pivot = random.randint(left, right)
+            points[pivot], points[right] = points[right], points[pivot]
+            pivot = right
+            pivot_dist = euc_dist(points[pivot])
+            
+            i = left-1
+            
+            while left<right:
+                if euc_dist(points[left]) <= pivot_dist:
+                    i+=1
+                    points[i], points[left] = points[left], points[i]
+                left+=1
+            i+=1
+            points[pivot], points[i] = points[i], points[pivot]
+            return i
+        
+        start, end = 0, len(points)-1
+        pivot = end+1
+        while pivot!=k:
+            pivot = partition(start, end)
+            if pivot > k:
+                end = pivot - 1
+            else:
+                start = pivot+1
+        return points[:k]
                 
         
