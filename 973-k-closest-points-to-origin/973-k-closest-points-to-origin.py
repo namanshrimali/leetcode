20926b17964f08dd25f21a737c50145ecacf2754
Naman Shrimali <namanshrimali@gmail.com>
2022-02-13 10:36:09 -0500

    Time: 1402 ms (16.20%), Space: 20.6 MB (12.64%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -2,32 +2,41 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def euc_dist(point):
             return point[0]**2+point[1]**2
+        heap = []
+        for point in points:
+            heapq.heappush(heap, (euc_dist(point), point))
+        sol = []
+        while heap and k:
+            _, point = heapq.heappop(heap)
+            sol.append(point)
+            k-=1
+        return sol
         
-        def partition(left, right):
-            pivot = random.randint(left, right)
-            points[pivot], points[right] = points[right], points[pivot]
-            pivot = right
-            pivot_dist = euc_dist(points[pivot])
+#         def partition(left, right):
+#             pivot = random.randint(left, right)
+#             points[pivot], points[right] = points[right], points[pivot]
+#             pivot = right
+#             pivot_dist = euc_dist(points[pivot])
             
-            i = left-1
+#             i = left-1
             
-            while left<right:
-                if euc_dist(points[left]) <= pivot_dist:
-                    i+=1
-                    points[i], points[left] = points[left], points[i]
-                left+=1
-            i+=1
-            points[pivot], points[i] = points[i], points[pivot]
-            return i
+#             while left<right:
+#                 if euc_dist(points[left]) <= pivot_dist:
+#                     i+=1
+#                     points[i], points[left] = points[left], points[i]
+#                 left+=1
+#             i+=1
+#             points[pivot], points[i] = points[i], points[pivot]
+#             return i
         
-        start, end = 0, len(points)-1
-        pivot = end+1
-        while pivot!=k:
-            pivot = partition(start, end)
-            if pivot > k:
-                end = pivot - 1
-            else:
-                start = pivot+1
-        return points[:k]
+#         start, end = 0, len(points)-1
+#         pivot = end+1
+#         while pivot!=k:
+#             pivot = partition(start, end)
+#             if pivot > k:
+#                 end = pivot - 1
+#             else:
+#                 start = pivot+1
+#         return points[:k]
                 
         
