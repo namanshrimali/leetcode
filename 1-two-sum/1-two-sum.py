6f934e76f677823798c18decb0b5375cd570b774
Naman Shrimali <namanshrimali@gmail.com>
2022-02-14 09:02:47 -0500

    Time: 94 ms (59.29%), Space: 15.2 MB (59.34%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,15 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for index, num in enumerate(nums):
+#             for another_index, another_num in enumerate(nums[index+1:]):
+#                 if num+another_num == target:
+#                     return [index, index+another_index+1] 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = {}
-        for idx in range(len(nums)):
-            second = target-nums[idx]
-            if second in hash_map:
-                return [hash_map[second],idx]
-            else:
-                hash_map[nums[idx]] = idx
+        valueMap = {}
+        for index, num in enumerate(nums):
+            another_value = target-num
+            if another_value in valueMap:
+                return [valueMap[another_value], index]
+            valueMap[num] = index
