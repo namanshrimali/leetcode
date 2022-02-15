18c21613bd6ef02d6001991f07e7c379396eeb39
Naman Shrimali <namanshrimali@gmail.com>
2022-02-15 17:45:56 -0500

    Time: 58 ms (12.79%), Space: 13.8 MB (99.58%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -1,14 +1,18 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
+        # if number is highest on the right, no swapping of that number
+        # if number is not highest, swap it with highest number on the right
         num = list(str(num))
-        left, right = 0, 0
-        right_max_idx = len(num)-1
-        for i in range(len(num)-2, -1, -1):
-            if num[i] > num[right_max_idx]:
-                right_max_idx = i
-            elif num[i] < num[right_max_idx]:
-                left = i
-                right = right_max_idx
-        num[left], num[right] = num[right], num[left]
-        return int(''.join(num))
-                
+        L = len(num)
+        max_arr = [0] * L
+        max_arr[L-1] = L-1
+        for i in range(L-2, -1, -1):
+            if num[i] > num[max_arr[i+1]]:
+                max_arr[i] = i
+            else:
+                max_arr[i] = max_arr[i+1]
+        for i in range(len(num)):
+            if num[i] < num[max_arr[i]]:
+                num[i], num[max_arr[i]]  = num[max_arr[i]], num[i]
+                break
+        return ''.join(num)
