ad7e1c41fc2367b7047765b14451b562ae28628b
Naman Shrimali <namanshrimali@gmail.com>
2022-02-14 20:14:33 -0500

    Time: 32 ms (99.39%), Space: 13.9 MB (94.32%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -4,20 +4,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i = len(nums)-2
-        while(i>=0 and nums[i]>=nums[i+1]):
+        while i > -1:
+            if nums[i] < nums[i+1]:
+                break
             i-=1
-            
-        if i>=0:
-            next_max_idx = i+1
-            for j in range(i+1, len(nums)):
-                if nums[j] <= nums[next_max_idx] and nums[j]>nums[i]:
-                    next_max_idx = j
-            nums[i], nums[next_max_idx] = nums[next_max_idx], nums[i]
+        # the i'th number is the smallest on the right hand side
+        # finding the next largest number
+        if i != -1:
+            # number at i is the largest number in array, and array is in descending order
+            # if not that, finding the number which is just bigger than number at i
+            next_largest = len(nums)-1
+            while nums[next_largest] <= nums[i]:
+                next_largest-=1
+            nums[i], nums[next_largest] = nums[next_largest], nums[i]
         
-        i,j = i+1, len(nums)-1
-        while(i<j):
-            nums[i], nums[j] = nums[j], nums[i]
-            i+=1
-            j-=1
-        
+        left, right = i+1, len(nums)-1
+        while left<right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left+=1
+            right-=1
         return nums
