d3a502d1a8eeeaae4131ddc00e09a10326129c21
Naman Shrimali <namanshrimali@gmail.com>
2022-02-27 10:20:57 -0500

    Time: 1321 ms (66.55%), Space: 24.9 MB (66.98%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,17 +1,18 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        time, L = 0, len(colors)
-        def remove_same_baloons(idx, color):
-            total_time = max_time = 0
-            
-            while idx < L and colors[idx]==color:
-                total_time+=neededTime[idx]
-                max_time = max(max_time, neededTime[idx])
+        def remove_adj_colours(idx, colour):
+            total_time, max_time = 0, 0
+            while idx < len(colors) and colors[idx] == colour:
+                total_time += neededTime[idx]
+                if neededTime[idx] > max_time:
+                    max_time = neededTime[idx]
                 idx+=1
-            return total_time - max_time, idx
-        idx = 0
-        while idx < L:
-            min_time, idx = remove_same_baloons(idx, colors[idx])
-            time+=min_time
+            return total_time - max_time, idx-1
+        total_time, i = 0, 0
+        while i < len(colors):
+            time_taken, i = remove_adj_colours(i, colors[i])
+            total_time += time_taken
+            i+=1
+        return total_time
+        
             
-        return time
