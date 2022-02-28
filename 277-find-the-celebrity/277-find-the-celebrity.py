710d1ada746a1ef54500ff75827e26f78a9e2568
Naman Shrimali <namanshrimali@gmail.com>
2022-02-28 15:22:47 -0500

    Time: 1716 ms (86.31%), Space: 14.2 MB (59.27%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -4,17 +4,17 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
-        celebrity_candidate = 0
-        for i in range(1, n):
-            if knows(celebrity_candidate, i):
-                # celebrity_candidate is not a celebrity, but i might be
-                celebrity_candidate = i
-        for i in range(n):
-            if i == celebrity_candidate:
-                continue
-            if knows(celebrity_candidate, i) or not knows(i, celebrity_candidate):
-                # either celebrity_candidate knows i or i does not know celebrity_candidate
-                return -1
-        return celebrity_candidate
-                
+        candidate_celebrity = 0
+        for curr in range(1, n):
+            if knows(candidate_celebrity, curr):
+                # candidate is not a celebrity
+                candidate_celebrity = curr
         
+        # we've our candidate celebrity, now we'll check whether everyone knows him and make sure
+        # candidate doesn't know anyone
+        for i in range(n):
+            if i == candidate_celebrity:
+                continue
+            if not(knows(i, candidate_celebrity) and not knows(candidate_celebrity, i)):
+                return -1
+        return candidate_celebrity
