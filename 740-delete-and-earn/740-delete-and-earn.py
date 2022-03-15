17d9d13bc11d13a3c99622efdd94884b69f50e90
Naman Shrimali <namanshrimali@gmail.com>
2022-03-15 17:47:04 -0400

    Time: 91 ms (44.61%), Space: 14.6 MB (32.35%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -5,14 +5,10 @@ class Solution:
         for num in nums:
             count_map[num] += 1
             max_point = max(max_point, num)
-        
-        def max_points(num):
-            if num in known:
-                return known[num]
-            if num == 0 or num == 1:
-                return count_map.get(num, 0)
-            known[num] = max(max_points(num-1), max_points(num-2)+ num * count_map[num])
-            return known[num]
-        
-        return max_points(max_point)
+        prev, second_prev = 0, 0
+        for i in range(max_point+1):
+            curr = max(prev, second_prev + i * count_map[i])
+            second_prev = prev
+            prev = curr
+        return curr
                         
