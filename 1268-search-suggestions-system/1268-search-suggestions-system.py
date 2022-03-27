71a737d79c167d0335bc7f9f2dc2c1166d327211
Naman Shrimali <namanshrimali@gmail.com>
2022-03-27 12:39:32 -0400

    Time: 3430 ms (5.02%), Space: 23.9 MB (5.05%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,24 +1,53 @@
+class Node:
+    def __init__(self):
+        self.children = [None for i in range(26)]
+        self.is_word = False
+    
+class Trie:
+    def __init__(self):
+        self.root = Node()
+        
+    def insert(self, word):
+        curr = self.root
+        for char in word:
+            char_idx = ord(char) - ord('a')
+            
+            if curr.children[char_idx] is None:
+                curr.children[char_idx] = Node()
+            curr = curr.children[char_idx]
+        curr.is_word = True
+    
+        
+    def get_word_starting_with(self, prefix):
+        def dfs(curr, word):
+            nonlocal answer
+            if len(answer) == 3:
+                return
+            if curr.is_word:
+                answer.append(word)
+
+            for i in range(26):
+                if curr.children[i] != None:
+                    dfs(curr.children[i], word+chr(ord('a')+i))
+        
+        curr = self.root
+        answer = []
+        for char in prefix:
+            char_idx = ord(char)-ord('a')
+            if curr.children[char_idx] == None:
+                return answer
+            curr = curr.children[char_idx]
+        dfs(curr, prefix)
+        return answer
+                
+        
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
-        def binary_search(prefix, left, right = len(products)-1):
-            prefix_len = len(prefix)
-            while left < right:
-                mid = (left+right)//2
-                if products[mid][:prefix_len] < prefix:
-                    left = mid+1
-                else:
-                    right = mid
-            return right
-        
-        start = 0
-        sol = []
+        trie = Trie()
+        result = []
+        for product in products:
+            trie.insert(product)
         for i in range(len(searchWord)):
-            prefix = searchWord[:i+1]
-            start = binary_search(prefix, start)
-            sub_sol = []
-            for j in range(3):
-                if start+j < len(products) and products[start+j][:i+1] == prefix:
-                    sub_sol.append(products[start+j])
-            sol.append(sub_sol)
-        return sol
+            result.append(trie.get_word_starting_with(searchWord[:i+1]))
+        return result
+        
