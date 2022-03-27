ce059101d854433681c568c3662bad0a0ff3f8d8
Naman Shrimali <namanshrimali@gmail.com>
2022-03-27 14:08:06 -0400

    Time: 112 ms (5.42%), Space: 14.1 MB (42.16%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -1,11 +1,38 @@
 class Solution:
-    def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        letter_log, digit_log = [], []
-        for log in logs:
-            log = log.split(" ")
-            if log[1].isdigit():
-                digit_log.append(' '.join(log))
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:    
+        
+        def compare_fn(item1, item2):
+            item1 = item1.split()
+            item2 = item2.split()
+            
+            if len(item1) == len(item2) == 1:
+                if item1 < item2:
+                    return -1
+                elif item1 > item2:
+                    return 1
+                else:
+                    return 0
+            
+            if item1[1:] == item2[1:]:
+                return compare_fn(item1[0], item2[0])
             else:
-                letter_log.append(' '.join(log))
-        letter_log.sort(key = lambda letter: (letter.split(' ')[1:], letter[:4]))
-        return letter_log+digit_log
+                if item1[1:] > item2[1:]:
+                    return 1
+                elif item1[1:] < item2[1:]:
+                    return -1
+                else:
+                    return 0
+            
+        digit_logs = []
+        letter_logs = []
+        
+        for log in logs:
+            log_arr = log.split()
+            if log_arr[1].isdigit():
+                digit_logs.append(log)
+            else:
+                letter_logs.append(log)
+                
+        letter_logs.sort(key = functools.cmp_to_key(compare_fn))
+        return letter_logs+digit_logs
+        
