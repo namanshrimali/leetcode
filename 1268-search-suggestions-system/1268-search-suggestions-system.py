0287baefd6b81df1fe295b1a24367fbbb1990dad
Naman Shrimali <namanshrimali@gmail.com>
2022-03-30 15:17:57 -0400

    Time: 2552 ms (5.01%), Space: 23.9 MB (5.32%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,53 +1,58 @@
-class Node:
+def get_index(char):
+    return ord(char) - ord('a')
+
+class Node:    
     def __init__(self):
         self.children = [None for i in range(26)]
         self.is_word = False
     
-class Trie:
-    def __init__(self):
-        self.root = Node()
-        
-    def insert(self, word):
-        curr = self.root
+    def add_word(self, word):
+        node = self
         for char in word:
-            char_idx = ord(char) - ord('a')
-            
-            if curr.children[char_idx] is None:
-                curr.children[char_idx] = Node()
-            curr = curr.children[char_idx]
-        curr.is_word = True
+            idx = get_index(char)
+            if node.children[idx] is None:
+                node.children[idx] = Node()
+            node = node.children[idx]
+        node.is_word = True
     
-        
-    def get_word_starting_with(self, prefix):
-        def dfs(curr, word):
-            nonlocal answer
-            if len(answer) == 3:
-                return
-            if curr.is_word:
-                answer.append(word)
-
-            for i in range(26):
-                if curr.children[i] != None:
-                    dfs(curr.children[i], word+chr(ord('a')+i))
-        
-        curr = self.root
-        answer = []
-        for char in prefix:
-            char_idx = ord(char)-ord('a')
-            if curr.children[char_idx] == None:
-                return answer
-            curr = curr.children[char_idx]
-        dfs(curr, prefix)
-        return answer
-                
-        
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        trie = Trie()
-        result = []
+        end_result = []
+        
+        def dfs(node, word):
+            nonlocal top_matching
+            
+            if len(top_matching) == 3:
+                return
+            
+            if node.is_word:
+                top_matching.append(word)
+            
+            for i in range(26):
+                if node.children[i] != None:
+                    dfs(node.children[i], word+chr(ord('a') + i))
+            
+        
+        root = Node()
         for product in products:
-            trie.insert(product)
-        for i in range(len(searchWord)):
-            result.append(trie.get_word_starting_with(searchWord[:i+1]))
-        return result
+            root.add_word(product)
+        
+        node = root
+        
+        for i, letter in enumerate(searchWord):
+            idx = get_index(letter)
+            node = node.children[idx]
+            
+            if node == None:
+                for j in range(len(searchWord)-i):
+                    end_result.append([])
+                break
+            
+            top_matching = []
+            dfs(node, searchWord[:i+1])
+            end_result.append(top_matching[:3])
+    
+        return end_result
+            
+        
         
