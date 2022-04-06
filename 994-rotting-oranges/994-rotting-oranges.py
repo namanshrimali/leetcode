5b4800ea3cad929e835160f3176aefa1321cb76b
Naman Shrimali <namanshrimali@gmail.com>
2022-04-05 20:09:09 -0400

    Time: 80 ms (40.68%), Space: 13.9 MB (93.18%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,30 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         m, n = len(grid), len(grid[0])
-        
-            
+        total_fresh_oranges = 0
         rotten = collections.deque([])
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 2: # rotten orange found
-                    rotten.append([row, col])
-        time = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    total_fresh_oranges +=1
+                if grid[i][j] == 2:
+                    rotten.append((i, j))
+        if total_fresh_oranges == 0:
+            return 0
+        timer = -1
         while rotten:
-            rot_len = len(rotten)
-            for _ in range(rot_len):
-                row, col = rotten.popleft()
-                
-                for row_dir, col_dir in directions:
-                    row_dir, col_dir = row+row_dir, col+col_dir
-                    if -1 < row_dir < m and -1 < col_dir < n and grid[row_dir][col_dir] == 1:
-                        grid[row_dir][col_dir] = 2
-                        rotten.append([row_dir, col_dir])
-            if rotten:
-                time+=1
-
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == 1: # fresh orange found
-                    return -1
-        return time
+            deque_len = len(rotten)
+            for _ in range(deque_len):
+                i, j = rotten.popleft()
+                for x, y in directions:
+                    if -1<i+x<m and -1<j+y<n and grid[i+x][j+y] == 1:
+                        grid[i+x][j+y] = 2
+                        total_fresh_oranges -=1
+                        rotten.append((i+x, j+y))
+            timer+=1
+        
+        return -1 if total_fresh_oranges > 0 else timer
+        
