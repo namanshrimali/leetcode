9af4ef2fb71b15499ff2134ff72c20a1b4110914
Naman Shrimali <namanshrimali@gmail.com>
2022-04-06 14:01:37 -0400

    Time: 441 ms (42.51%), Space: 15.6 MB (6.86%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
@@ -1,11 +1,13 @@
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        heapq.heapify(sticks)
-        total = 0
-        while len(sticks)>=2:
-            smallest, second_smallest = heapq.heappop(sticks), heapq.heappop(sticks)
-            bigger_stick = smallest+second_smallest
-            total+= bigger_stick
-            heapq.heappush(sticks, bigger_stick)
-        return total
-        
+        heap = []
+        for stick in sticks:
+            heapq.heappush(heap, stick)
+        total_cost = 0
+        while len(heap)>1:
+            first, second = heapq.heappop(heap), heapq.heappop(heap)
+            final_stick = first+second
+            total_cost += final_stick
+            heapq.heappush(heap, final_stick)
+        return total_cost
+            
