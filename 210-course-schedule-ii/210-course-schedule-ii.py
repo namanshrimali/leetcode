22f4d8d8cd85b14dd36f27a02dab07ed868e96ac
Naman Shrimali <namanshrimali@gmail.com>
2022-04-06 13:30:48 -0400

    Time: 225 ms (11.69%), Space: 15.7 MB (50.85%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,21 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        child_ref, indegree = {}, {}
+        def is_node_parent(i):
+            return i not in rel_map or len(rel_map[i][0]) == 0
+
+        rel_map = {}
         for course, prereq in prerequisites:
-            child_ref[prereq] = child_ref.get(prereq, [])
-            child_ref[prereq].append(course)
-            indegree[course] = indegree.get(course, 0)+1
-        queue = collections.deque([k for k in range(numCourses) if k not in indegree])
-        sol = []
+            if course not in rel_map:
+                rel_map[course] = [set(), set()]
+            if prereq not in rel_map:
+                rel_map[prereq] = [set(), set()]
+            rel_map[course][0].add(prereq)
+            rel_map[prereq][1].add(course)
         
-        while queue:
-            vertex = queue.popleft()
-            sol.append(vertex)
-            if vertex in child_ref:
-                for child in child_ref[vertex]:
-                    indegree[child]-=1
-                
-                    if indegree[child]==0:
-                        queue.append(child)
-        return sol if len(sol) == numCourses else []
+        deque = collections.deque([])
+        answer = []
+        
+        for i in range(numCourses):
+            if is_node_parent(i):
+                deque.append(i)
+        while deque:
+            node = deque.popleft()
+            answer.append(node)
+            for children in rel_map.get(node, [[], []])[1]:
+                rel_map[children][0].remove(node)
+                if is_node_parent(children):
+                    deque.append(children)
+        return [] if len(answer) != numCourses else answer
             
+        
