ad24640a2737bea8c443e158c14e9f921d04cdf1
Naman Shrimali <namanshrimali@gmail.com>
2022-04-07 18:28:24 -0400

    Time: 96 ms (67.35%), Space: 17.5 MB (52.52%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,16 +1,12 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if len(intervals) == 1:
-            return 1
-        intervals.sort(key= lambda a: a[0])
-        free_rooms = [] # maintaining min heap of rooms with earliest end time  
-        heapq.heappush(free_rooms, intervals[0][1])
+        intervals.sort(key = lambda interval : interval[0])
+        heap = []
         
-        for start, end in intervals[1:]:
-            if free_rooms[0] <= start:
-                heapq.heappop(free_rooms)
-            heapq.heappush(free_rooms, end)
-        return len(free_rooms)
-
-        
-        
+        for start, end in intervals:
+            
+            if heap and heap[0][0] <= start:
+                heapq.heappop(heap)
+            heapq.heappush(heap, [end, start])
+            
+        return len(heap)
