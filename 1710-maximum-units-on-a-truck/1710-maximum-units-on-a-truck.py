9aabba200c7f3680bcb95e2160f3397b3f45ff81
Naman Shrimali <namanshrimali@gmail.com>
2022-04-07 18:45:23 -0400

    Time: 1352 ms (9.17%), Space: 14.4 MB (86.73%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -3,16 +3,18 @@ class Solution:
         heap = []
         for qnty, units in boxTypes:
             heapq.heappush(heap, [-units, qnty])
+        
         total_units = 0
         
-        while heap and truckSize:
-            units, qnty = -heap[0][0], heap[0][1]
-            req = min(qnty, truckSize)
-            qnty -= req
-            truckSize -= req
-            total_units += req * units 
+        while truckSize and heap:
+            units_per_box, qnty = -heap[0][0], heap[0][1]
             
+            while qnty and truckSize:
+                total_units += units_per_box
+                truckSize -=1
+                qnty-=1
+                
             if qnty == 0:
                 heapq.heappop(heap)
-                
+        
         return total_units
