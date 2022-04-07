c344219e1998bf432534ab097b0ace18cf2d21c1
Naman Shrimali <namanshrimali@gmail.com>
2022-04-07 09:56:47 -0400

    Time: 988 ms (15.00%), Space: 23.3 MB (5.28%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,58 +1,53 @@
-def get_index(char):
-    return ord(char) - ord('a')
 
-class Node:    
-    def __init__(self):
-        self.children = [None for i in range(26)]
+def get_letter_idx(letter):
+    return ord(letter)-ord('a')
+
+def get_letter_from_idx(idx):
+    return chr(ord('a') + idx)
+
+class Node:
+    def __init__(self, letter):
+        self.children = [None]*26
         self.is_word = False
+        self.letter = letter
     
-    def add_word(self, word):
-        node = self
-        for char in word:
-            idx = get_index(char)
-            if node.children[idx] is None:
-                node.children[idx] = Node()
-            node = node.children[idx]
-        node.is_word = True
+    def insert_product(self, product):
+        root = self
+        for letter in product:
+            letter_idx = get_letter_idx(letter)
+            if root.children[letter_idx] == None:
+                root.children[letter_idx] = Node(get_letter_from_idx(letter_idx))
+            root = root.children[letter_idx]
+        root.is_word = True
     
+    def get_all_products(self, word_till_now):
+        products = []
+        def dfs(node, word):
+            nonlocal products
+            if len(products) == 3:
+                return
+            if node.is_word:
+                products.append(word)
+            
+            for child in node.children:
+                if child != None:
+                    dfs(child, word + child.letter)
+        dfs(self, word_till_now + self.letter)
+        return products
+                
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        end_result = []
-        
-        def dfs(node, word):
-            nonlocal top_matching
-            
-            if len(top_matching) == 3:
-                return
-            
-            if node.is_word:
-                top_matching.append(word)
-            
-            for i in range(26):
-                if node.children[i] != None:
-                    dfs(node.children[i], word+chr(ord('a') + i))
-            
-        
-        root = Node()
+        root = Node('')
         for product in products:
-            root.add_word(product)
-        
-        node = root
-        
-        for i, letter in enumerate(searchWord):
-            idx = get_index(letter)
-            node = node.children[idx]
-            
-            if node == None:
-                for j in range(len(searchWord)-i):
-                    end_result.append([])
+            root.insert_product(product)
+        answer = []
+        for i in range(len(searchWord)):
+            letter = searchWord[i]
+            root = root.children[get_letter_idx(letter)]
+            if root is None:
+                while i < len(searchWord):
+                    answer.append([])
+                    i+=1
                 break
-            
-            top_matching = []
-            dfs(node, searchWord[:i+1])
-            end_result.append(top_matching[:3])
-    
-        return end_result
-            
-        
-        
+            answer.append(root.get_all_products(searchWord[:i]))
+        return answer
