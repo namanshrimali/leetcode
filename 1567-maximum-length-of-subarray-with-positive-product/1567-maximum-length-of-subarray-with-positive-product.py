9fb85c346225df6f10e572b59e9a5810f1ebc549
Naman Shrimali <namanshrimali@gmail.com>
2022-04-07 22:23:15 -0400

    Time: 872 ms (46.20%), Space: 28.4 MB (41.37%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -1,28 +1,20 @@
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
-        last, curr, no_of_negatives = 0, 0, 0
-        non_zero_subarrays = []
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                non_zero_subarrays.append((nums[last:curr], no_of_negatives))
-                last = curr+1
-                no_of_negatives = 0
-            if nums[i] < 0:
-                no_of_negatives +=1
-            curr+=1
-        if last < len(nums):
-            non_zero_subarrays.append((nums[last:], no_of_negatives))
-        
-        max_len = 0
-        for array, no_negatives in non_zero_subarrays:
-            array_len = len(array)
-            if no_negatives % 2 == 0:
-                if array_len > max_len:
-                    max_len = array_len
-            else:
-                for i in range(array_len):
-                    if array[i] < 0:
-                        sub_arr_max = max(i, array_len-i-1)
-                        max_len = max(max_len, sub_arr_max)
-                    
-        return max_len
+        n = len(nums)
+        pos_arr, neg_arr = [0]*n, [0]*n
+        if nums[0] > 0: 
+            pos_arr[0] = 1
+        if nums[0] < 0:
+            neg_arr[0] = 1
+            
+        for i in range(1, n):
+            num = nums[i]
+            if num > 0:
+                pos_arr[i] = pos_arr[i-1] + 1
+                neg_arr[i] = neg_arr[i-1] + 1 if neg_arr[i-1] > 0 else 0
+    
+            elif num < 0:
+                pos_arr[i] = neg_arr[i-1] + 1 if neg_arr[i-1] > 0 else 0
+                neg_arr[i] = pos_arr[i-1] + 1
+        return max(pos_arr)
+                
