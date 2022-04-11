b63d79d11118816a33eef4cfccf4cf3cced75a58
Naman Shrimali <namanshrimali@gmail.com>
2022-04-11 13:52:07 -0400

    Time: 328 ms (87.50%), Space: 28.6 MB (21.56%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,20 +1,21 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
-        MOD = 1_000_000_000+7
-        
+        MOD = 10**9+7
         horizontalCuts.sort(), verticalCuts.sort()
+        
+        max_height, max_width = -inf, -inf
         
         horizontalCuts = [0] + horizontalCuts + [h]
         verticalCuts = [0] + verticalCuts + [w]
         
-        max_hor, max_ver = -inf, -inf
-        
         for i in range(1, len(horizontalCuts)):
-            max_hor = max(max_hor, horizontalCuts[i] - horizontalCuts[i-1])
+            local_max = horizontalCuts[i] - horizontalCuts[i-1]
+            max_height = max(max_height, local_max)
         
         for i in range(1, len(verticalCuts)):
-            max_ver = max(max_ver, verticalCuts[i] - verticalCuts[i-1])
-            
-        return (max_hor*max_ver)%MOD
+            local_max = verticalCuts[i]-verticalCuts[i-1]
+            max_width = max(max_width, local_max)
         
+        return (max_width * max_height)%MOD
+            
         
