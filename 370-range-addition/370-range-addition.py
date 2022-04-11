fd20b3b43010cc9741688c90f637bf22e734723f
Naman Shrimali <namanshrimali@gmail.com>
2022-04-11 10:18:16 -0400

    Time: 385 ms (6.06%), Space: 22.8 MB (55.01%) - LeetHub

--- 370-range-addition/370-range-addition.py
+++ 370-range-addition/370-range-addition.py
@@ -1,12 +1,14 @@
 class Solution:
     def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
-        sol = [0]*length
-        for start, end, inc in updates:
-            sol[start]+=inc
-            if end+1 < length:
-                sol[end+1]-=inc
+        output = [0]*length
+        for start_idx, end_idx, inc in updates:
+            output[start_idx]+= inc
+            if end_idx+1 < length:
+                output[end_idx+1] += -inc
         for i in range(1, length):
-            sol[i]+= sol[i-1]
+            output[i] += output[i-1]
+        return output
+    
         
-        return sol
+        
         
