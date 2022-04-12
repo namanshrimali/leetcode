be30bf2c7bde523bfb8af2052573b291fcd36b34
Naman Shrimali <namanshrimali@gmail.com>
2022-04-12 00:22:53 -0400

    Time: 837 ms (51.82%), Space: 27.9 MB (72.29%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -1,20 +1,27 @@
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
-        n = len(nums)
-        pos_arr, neg_arr = [0]*n, [0]*n
-        if nums[0] > 0: 
-            pos_arr[0] = 1
-        if nums[0] < 0:
-            neg_arr[0] = 1
-            
-        for i in range(1, n):
-            num = nums[i]
-            if num > 0:
-                pos_arr[i] = pos_arr[i-1] + 1
-                neg_arr[i] = neg_arr[i-1] + 1 if neg_arr[i-1] > 0 else 0
-    
-            elif num < 0:
-                pos_arr[i] = neg_arr[i-1] + 1 if neg_arr[i-1] > 0 else 0
-                neg_arr[i] = pos_arr[i-1] + 1
-        return max(pos_arr)
-                
+        last, curr, no_of_negatives = 0, 0, 0
+        non_zero_subarrays = []
+        for i, num in enumerate(nums):
+            if num == 0:
+                non_zero_subarrays.append((nums[last:curr], no_of_negatives))
+                last = curr + 1
+                no_of_negatives = 0
+            if num < 0:
+                no_of_negatives += 1
+            curr += 1    
+        
+        if last < len(nums):
+            non_zero_subarrays.append((nums[last:], no_of_negatives))
+        max_pos_subarr = 0
+        
+        for arr, no_of_negatives in non_zero_subarrays:
+            if no_of_negatives % 2 == 0:
+                max_pos_subarr = max(max_pos_subarr, len(arr))
+            else:
+                local_max = 0
+                for i in range(len(arr)):
+                    if arr[i] < 0:
+                        local_max = max(i, len(arr)-i-1)
+                        max_pos_subarr = max(max_pos_subarr, local_max)
+        return max_pos_subarr
