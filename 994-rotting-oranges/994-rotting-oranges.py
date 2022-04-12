abc23c85964c87d3c01e10113637d7b3ca164a49
Naman Shrimali <namanshrimali@gmail.com>
2022-04-12 00:25:54 -0400

    Time: 72 ms (53.14%), Space: 13.9 MB (50.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -2,29 +2,27 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         m, n = len(grid), len(grid[0])
-        
-        def bfs(deque, fresh_oranges):
-            counter = -1
-            while deque:
-                que_len = len(deque)
-                for _ in range(que_len):
-                    curr_x, curr_y = deque.popleft()
-                    for x, y in directions:
-                        new_x, new_y = curr_x+x, curr_y+y
-                        if -1 < new_x < m and -1 < new_y < n and grid[new_x][new_y] == 1:
-                            grid[new_x][new_y] = 2
-                            fresh_oranges -= 1
-                            deque.append((new_x, new_y))
-                counter+=1
-            return counter if fresh_oranges == 0 else -1
-        
-        fresh_oranges, deque = 0, collections.deque([])
+        total_fresh_oranges = 0
+        rotten = collections.deque([])
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    fresh_oranges+=1
+                    total_fresh_oranges +=1
                 if grid[i][j] == 2:
-                    deque.append((i, j))
-        return bfs(deque, fresh_oranges) if fresh_oranges else 0
-                
+                    rotten.append((i, j))
+        if total_fresh_oranges == 0:
+            return 0
+        timer = -1
+        while rotten:
+            deque_len = len(rotten)
+            for _ in range(deque_len):
+                i, j = rotten.popleft()
+                for x, y in directions:
+                    if -1<i+x<m and -1<j+y<n and grid[i+x][j+y] == 1:
+                        grid[i+x][j+y] = 2
+                        total_fresh_oranges -=1
+                        rotten.append((i+x, j+y))
+            timer+=1
+        
+        return -1 if total_fresh_oranges > 0 else timer
         
