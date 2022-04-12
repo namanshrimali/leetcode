7ee5dc65f4d63201fd9a7c45af8f874a32809002
Naman Shrimali <namanshrimali@gmail.com>
2022-04-11 22:06:06 -0400

    Time: 38 ms (72.80%), Space: 13.9 MB (18.36%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,18 +1,17 @@
-import sys
 class Solution:
     def reverse(self, x: int) -> int:
-        rev = 0
-        max_size = 2147483647
-        min_size = -2147483648
-        sign = -1 if x<0 else 1
-        while x!=0:
-            pop = (sign * x)%10
-            pop *= sign
-            x = ((sign*x)//10)*sign
-            if (rev > max_size//10 or (rev == max_size//10 and pop > 7)):
-                return 0
-            if (rev < -(-min_size//10) or (rev == -(-min_size//10)) and pop < -8):
-                return 0
-            rev = rev*10 + pop
-        return rev
+        MAX_VAL = 2**31-1
+        MIN_VAL = -2**31
+        sign = 1 if x > 0 else -1
         
+        rev = 0
+        while x!=0:
+            rem = sign*((sign*x)%10)
+            if rev > MAX_VAL//10 or (rev == MAX_VAL//10 and rem > 7):
+                return 0
+            
+            if rev < -(-MIN_VAL//10) or (rev == -(-MIN_VAL//10) and rem < -8):
+                return 0
+            rev = rev*10 + rem
+            x = sign * ((sign*x)//10)
+        return rev
