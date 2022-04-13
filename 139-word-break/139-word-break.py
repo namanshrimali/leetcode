71be2c68d6e6233b2cb92aaa4f19a56bc70bf629
Naman Shrimali <namanshrimali@gmail.com>
2022-04-13 16:49:46 -0400

    Time: 40 ms (89.50%), Space: 14.3 MB (16.09%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,16 +1,27 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        visited, queue = set(), collections.deque([0])
-        L = len(s)
-        while queue:
-            start = queue.popleft()
-            if start in visited:
-                continue
-            if start == L:
-                return True
-            for end in range(start, L):
-                if s[start:end+1] in wordDict:
-                    queue.append(end+1)
-            visited.add(start)
-        return False
+        wordDict = set(wordDict)
+        found_list = [None]*len(s)
+        def backtrack(curr_idx):
+            nonlocal is_found
+            if is_found:
+                return
+            if curr_idx == len(s):
+                is_found = True
+                return
+            
+            if found_list[curr_idx] == False:
+                return
+            
+            for last_idx in range(curr_idx+1, len(s)+1):
+                
+                if s[curr_idx:last_idx] in wordDict:
+                    backtrack(last_idx)
+                if is_found:
+                    break
+            found_list[curr_idx] = is_found
+                
         
+        is_found = False
+        backtrack(0)
+        return is_found
