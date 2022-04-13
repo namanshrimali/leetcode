b3fe7452ffe6b4b6b7507bd72a7ed695782cf1d1
Naman Shrimali <namanshrimali@gmail.com>
2022-04-12 20:33:13 -0400

    Time: 99 ms (90.22%), Space: 15.2 MB (28.18%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.py
+++ 735-asteroid-collision/735-asteroid-collision.py
@@ -1,21 +1,22 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
-        for a in asteroids:
-            if a > 0:
-                stack.append(a)
-            else:
-                destroy_self = False
+        for asteroid in asteroids:
+            if asteroid < 0:
+                is_destroyed = False
                 while stack and stack[-1] > 0:
-                    top_a = stack[-1]
-                    if top_a >= -a:
-                        destroy_self = True
-                        if top_a == -a:
+                    top_asteroid = stack[-1]
+                    if top_asteroid >= -asteroid:
+                        if top_asteroid == -asteroid:
                             stack.pop()
-                        break
+                        is_destroyed = True
+                        break    
                     else:
                         stack.pop()
-                    
-                if not destroy_self:
-                    stack.append(a)
-        return stack
+                if not is_destroyed:
+                    stack.append(asteroid)
+            else:
+                stack.append(asteroid)
+        
+        return stack            
+        
