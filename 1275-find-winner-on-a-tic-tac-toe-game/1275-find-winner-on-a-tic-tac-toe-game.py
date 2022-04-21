4e61e6199e18470f5c6129a90752152faf3e9617
Naman Shrimali <namanshrimali@gmail.com>
2022-04-21 14:19:05 -0400

    Time: 27 ms (98.32%), Space: 13.9 MB (85.33%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,26 +1,31 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        diagonal, anti_diagonal = 0, 0
-        row, col = [0, 0, 0], [0, 0, 0]
+        might_be_pending = len(moves) < 9
+        curr_player = 1 # A is 1, B is -1
+        winner = None
+        row_total = [0]*3
+        col_total = [0]*3
+        diag_total = 0
+        anti_diag_total = 0
         
-        move = 1
-        for move_x, move_y in moves:
-            if move%2==1:
-                update = 1
-            else:
-                update = -1
-            row[move_x]+=update
-            col[move_y]+=update
-            if (move_x, move_y) in [(0, 0), (1,1), (2,2)]:
-                diagonal+=update
-            if (move_x, move_y) in [(0,2), (1,1), (2, 0)]:
-                anti_diagonal+=update
-            move+=1
-        if diagonal == 3 or anti_diagonal == 3 or 3 in row or 3 in col:
-            return "A"
-        elif diagonal == -3 or anti_diagonal == -3 or -3 in row or -3 in col:
-            return "B"
-        elif move<=9:
-            return "Pending"
+        for row, col in moves:
+            col_total[col] += curr_player
+            row_total[row] += curr_player
+            if row == col:
+                diag_total += curr_player
+            if row == 3-col-1:
+                anti_diag_total += curr_player
+            
+            if row_total[row] in (-3, 3) or col_total[col] in (-3, 3) or diag_total in (-3, 3) or anti_diag_total in (-3, 3):
+                winner = curr_player
+                break
+                
+            curr_player = - curr_player
+        
+        if winner:
+            return 'A' if winner == 1 else 'B'
+        elif might_be_pending:
+            return 'Pending'
         else:
-            return "Draw"
+            return 'Draw'
+            
