4cf674a21729702d10efea39b43561aaf5e5ac81
Naman Shrimali <namanshrimali@gmail.com>
2022-04-22 13:58:51 -0400

    Time: 757 ms (24.02%), Space: 18.9 MB (43.63%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,31 +1,18 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        L, MOD_VAL = len(arr), 10**9+7
-        left_stack, right_stack, sol_arr = [], [], []
+        MOD = 10**9 + 7
+        total = 0
         
-        def calc_left_max(i):
-            # returns the number of values that are strictly greater than or equals to given index i 
-            count = 1
-            while(left_stack and left_stack[-1][0]>arr[i]):
-                _, prev_count = left_stack.pop()
-                count+=prev_count
-            left_stack.append((arr[i], count))
-            return count
+        arr = [0] + arr
+        stack = [0]
+        n = len(arr)
+        result = [0] * n
+        for i in range(n):
+            while arr[stack[-1]] > arr[i]:
+                stack.pop()
+            j = stack[-1]
+            result[i] = result[j] + (i-j)*arr[i]
+
+            stack.append(i)        
         
-        def calc_right_max(i):
-            # returns the number of values that are strictly greater than or equals to given index i 
-            count = 1
-            while(right_stack and right_stack[-1][0]>=arr[i]):
-                _, prev_count = right_stack.pop()
-                count+=prev_count
-            right_stack.append((arr[i], count))
-            return count
-        
-        for i in range(L):
-            sol_arr.append(calc_left_max(i))
-        
-        for i in range(L-1, -1, -1):
-            sol_arr[i]*= calc_right_max(i)*arr[i]
-            
-        return sum(sol_arr)%MOD_VAL
-            
+        return sum(result) % MOD
