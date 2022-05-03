e0f6a491b3902a4325e73b5163b5b259487e02cc
Naman Shrimali <namanshrimali@gmail.com>
2022-05-03 08:53:25 -0400

    Time: 852 ms (45.14%), Space: 27.7 MB (75.73%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -1,27 +1,30 @@
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
-        last, curr, no_of_negatives = 0, 0, 0
+        n = len(nums)
+        last, total_negatives = 0, 0
         non_zero_subarrays = []
-        for i, num in enumerate(nums):
+        
+        for curr, num in enumerate(nums):
             if num == 0:
-                non_zero_subarrays.append((nums[last:curr], no_of_negatives))
+                non_zero_subarrays.append([nums[last:curr], total_negatives])
+                total_negatives = 0
                 last = curr + 1
-                no_of_negatives = 0
             if num < 0:
-                no_of_negatives += 1
-            curr += 1    
+                total_negatives += 1
         
-        if last < len(nums):
-            non_zero_subarrays.append((nums[last:], no_of_negatives))
-        max_pos_subarr = 0
+        if last < n:
+            non_zero_subarrays.append([nums[last:], total_negatives])
         
-        for arr, no_of_negatives in non_zero_subarrays:
-            if no_of_negatives % 2 == 0:
-                max_pos_subarr = max(max_pos_subarr, len(arr))
+        max_positive_subarray_len = 0
+        
+        for subarray, subarray_negatives in non_zero_subarrays:
+            if subarray_negatives % 2 == 0:
+                max_positive_subarray_len = max(max_positive_subarray_len, len(subarray))
             else:
-                local_max = 0
-                for i in range(len(arr)):
-                    if arr[i] < 0:
-                        local_max = max(i, len(arr)-i-1)
-                        max_pos_subarr = max(max_pos_subarr, local_max)
-        return max_pos_subarr
+                max_sub_subarray_len = 0
+                l = len(subarray)
+                for i in range(l):
+                    if subarray[i] < 0:
+                        max_sub_subarray_len = max(i, l - i - 1)
+                        max_positive_subarray_len  = max(max_positive_subarray_len, max_sub_subarray_len)
+        return max_positive_subarray_len
