0bccade2c6d4b621076a0b566e7f6533b1db8701
Naman Shrimali <namanshrimali@gmail.com>
2022-05-09 13:54:10 +0530

    Time: 673 ms (86.25%), Space: 19.7 MB (86.72%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,40 +1,40 @@
 class DisjointSet:
-    def __init__(self, N):
-        self.parents = [[1, i] for i in range(N+1)]
-    
+    def __init__(self, n):
+        self.parents = [i for i in range(n+1)]
+        self.weights = [1] * (n+1)
+        
     def union(self, a, b):
-        parent_a, parent_b = self.get_parent(a), self.get_parent(b)
+        parent_a, parent_b = self.find_parent(a), self.find_parent(b)
         if parent_a == parent_b:
             return
-        if self.parents[parent_a][0] > self.parents[parent_b][0]:
-            self.parents[parent_b][1] = parent_a
-            self.parents[parent_a][0] += self.parents[parent_b][0]
+        if self.weights[parent_a] > self.weights[parent_b]:
+            self.parents[parent_b] = parent_a
+            self.weights[parent_a] += self.weights[parent_b]
         else:
-            self.parents[parent_a][1] = parent_b
-            self.parents[parent_b][0] += self.parents[parent_a][0]
-        
+            self.parents[parent_a] = parent_b
+            self.weights[parent_b] += self.weights[parent_a]    
     
-    def get_parent(self, x):
-        while x != self.parents[x][1]:
-            self.parents[x][1] = self.parents[self.parents[x][1]][1]
-            x = self.parents[x][1]
-        return x
-        
-        
-    def in_in_same_group(self, a, b):
-        return self.get_parent(a) == self.get_parent(b)
+    def is_connected(self, a, b):
+        return self.find_parent(a) == self.find_parent(b)
     
+    def find_parent(self, a):
+        while a != self.parents[a]:
+            self.parents[a] = self.parents[self.parents[a]]
+            a = self.parents[a]
+        return a
+            
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        connections.sort(key=lambda a: a[2])
-        d_set = DisjointSet(n)
-        total_cost, total_paths = 0, n-1
-        for city_a, city_b, cost in connections:
-            if d_set.in_in_same_group(city_a, city_b):
+        connections.sort(key = lambda a: a[2])
+        total_cost, total_connections = 0, 0
+        
+        dj = DisjointSet(n)
+        
+        for x, y, cost in connections:
+            if dj.is_connected(x, y):
                 continue
-            d_set.union(city_a, city_b)
+            dj.union(x, y)
             total_cost += cost
-            total_paths -=1
-        return total_cost if total_paths == 0 else -1
-            
-            
+            total_connections += 1
+
+        return total_cost if total_connections == n-1 else -1
