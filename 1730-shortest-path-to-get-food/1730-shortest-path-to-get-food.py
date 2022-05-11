1ecbb130f5ac762497a64043382e402172bec1f6
Naman Shrimali <namanshrimali@gmail.com>
2022-05-11 17:06:01 +0530

    Time: 619 ms (59.09%), Space: 15.3 MB (62.52%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -1,27 +1,29 @@
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        curr_distance = -1
+        deque = collections.deque([])
         m, n = len(grid), len(grid[0])
         
-        def bfs(start_x, start_y):
-            grid[start_x][start_y] = 0
-            deque = collections.deque([(start_x, start_y)])
-            
-            while deque:
-                que_len = len(deque)
-                for _ in range(que_len):
-                    curr_x, curr_y = deque.popleft()
-                    for x, y in directions:
-                        new_x, new_y = curr_x + x, curr_y + y
-                        if -1 < new_x < m and -1 < new_y < n:
-                            if grid[new_x][new_y] == '#':
-                                return grid[curr_x][curr_y]+1
-                            if grid[new_x][new_y] == 'O':
-                                grid[new_x][new_y] = grid[curr_x][curr_y]+1
-                                deque.append((new_x, new_y))
-            return -1
-        
+        # iterating over grid to find starting point and placing it on deque
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '*':
-                    return bfs(i, j)
+                    deque.append((i, j))
+                    grid[i][j] = 'X'    # marking it as obstacle so that it won't be selected again in future
+                    break
+        
+        # finding food
+        while deque:
+            curr_distance += 1
+            for _ in range(len(deque)):
+                curr_x, curr_y = deque.popleft()
+                for x, y in directions:
+                    next_x, next_y = curr_x + x, curr_y + y
+                    if -1 < next_x < m and -1 < next_y < n and grid[next_x][next_y] in '#O':
+                        if grid[next_x][next_y] == '#':  # food found
+                            return curr_distance + 1
+                        deque.append((next_x, next_y))
+                        grid[next_x][next_y] = 'X'
+                
+        return -1
