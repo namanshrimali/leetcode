9575579420f6e81d28c5ec05a61d5e1f616c4c1f
Naman Shrimali <namanshrimali@gmail.com>
2022-05-24 11:38:18 +0530

    Time: 67 ms (83.47%), Space: 15.3 MB (24.28%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        valueMap = {}
-        for index, num in enumerate(nums):
-            another_value = target-num
-            if another_value in valueMap:
-                return [valueMap[another_value], index]
-            valueMap[num] = index
+        complements = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in complements:
+                break
+            complements[num] = i
+        return  [complements[complement], i]
+            
+        
