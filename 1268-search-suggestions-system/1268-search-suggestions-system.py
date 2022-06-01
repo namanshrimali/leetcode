67ddd45dc51bbe355c7e8c23dadeb4894741aeee
Naman Shrimali <namanshrimali@gmail.com>
2022-06-01 23:54:41 +0530

    Time: 3447 ms (5.02%), Space: 38.8 MB (5.04%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,53 +1,58 @@
+def get_char_idx(char):
+    return ord(char)-ord('a')
 
-def get_letter_idx(letter):
-    return ord(letter)-ord('a')
+def get_char_from_idx(idx):
+    return chr(idx + ord('a'))
 
-def get_letter_from_idx(idx):
-    return chr(ord('a') + idx)
-
-class Node:
-    def __init__(self, letter):
+class TrieNode:
+    def __init__(self):
         self.children = [None]*26
         self.is_word = False
-        self.letter = letter
+
+    def add_word(self, word):
+        curr_node = self
+        for char in word:
+            idx = get_char_idx(char)
+            if curr_node.children[idx] is None:
+                curr_node.children[idx] = TrieNode()
+            curr_node = curr_node.children[idx]
+        curr_node.is_word = True
     
-    def insert_product(self, product):
-        root = self
-        for letter in product:
-            letter_idx = get_letter_idx(letter)
-            if root.children[letter_idx] == None:
-                root.children[letter_idx] = Node(get_letter_from_idx(letter_idx))
-            root = root.children[letter_idx]
-        root.is_word = True
-    
-    def get_all_products(self, word_till_now):
-        products = []
-        def dfs(node, word):
-            nonlocal products
-            if len(products) == 3:
-                return
-            if node.is_word:
-                products.append(word)
+    def get_top_three_words(self, word_array, top_three_words = []):
+        if len(top_three_words) == 3:
+            return top_three_words
+        if self.is_word:
+            top_three_words.append(''.join(word_array))
             
-            for child in node.children:
-                if child != None:
-                    dfs(child, word + child.letter)
-        dfs(self, word_till_now + self.letter)
-        return products
-                
+        for i in range(len(self.children)):
+            if self.children[i] is not None:
+                top_three_words = self.children[i].get_top_three_words(word_array + [get_char_from_idx(i)], top_three_words)
+        return top_three_words
+                    
+            
+            
+            
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        root = Node('')
+        root = TrieNode()
+        
         for product in products:
-            root.insert_product(product)
-        answer = []
-        for i in range(len(searchWord)):
-            letter = searchWord[i]
-            root = root.children[get_letter_idx(letter)]
-            if root is None:
-                while i < len(searchWord):
-                    answer.append([])
-                    i+=1
+            root.add_word(product)
+        
+        result = []
+        curr_node = root
+        i = 0
+        while i < len(searchWord):
+            idx = get_char_idx(searchWord[i])
+            if curr_node.children[idx] is None:
                 break
-            answer.append(root.get_all_products(searchWord[:i]))
-        return answer
+            curr_node = curr_node.children[idx]
+            result.append(curr_node.get_top_three_words([searchWord[:i+1]], []))
+            i+=1
+            
+        for j in range(i, len(searchWord)):
+            result.append([])
+        
+        return result
+
+        
