ae666a4f705336b96f50091941b356a23daf7e02
Naman Shrimali <namanshrimali@gmail.com>
2022-06-05 11:51:08 -0400

    Time: 50 ms (91.81%), Space: 17.4 MB (56.69%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -6,9 +6,33 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        seen_nodes = set()
-        while head is not None and head not in seen_nodes:
-            seen_nodes.add(head)
-            head = head.next
-        return head
+        # seen_nodes = set()
+        # while head is not None and head not in seen_nodes:
+        #     seen_nodes.add(head)
+        #     head = head.next
+        # return head
+        if head is None or head.next is None:
+            return
         
+        
+        def get_intersecting_node():
+            tortoise, hare = head, head
+        
+            while hare and hare.next:
+                tortoise = tortoise.next
+                hare = hare.next.next
+                
+                if tortoise == hare:
+                    return tortoise
+            return None
+        
+        intersecting_node = get_intersecting_node()
+        if intersecting_node is None:
+            # no cycle present
+            return
+        ptr1, ptr2 = head, intersecting_node
+        
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+        return ptr1
