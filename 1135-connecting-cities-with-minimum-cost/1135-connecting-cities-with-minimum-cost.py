571060c9d89f274ba3e73f8ffb1c23dcc1251665
Naman Shrimali <namanshrimali@gmail.com>
2022-06-08 13:43:25 -0400

    Time: 1004 ms (31.20%), Space: 19.7 MB (96.42%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,40 +1,39 @@
 class DisjointSet:
     def __init__(self, n):
         self.parents = [i for i in range(n+1)]
-        self.weights = [1] * (n+1)
+        self.weights = [1]*(n+1)
+    
+    def union(self, city_1, city_2):
+        city_1_parent, city_2_parent = self.find_parent(city_1), self.find_parent(city_2)
         
-    def union(self, a, b):
-        parent_a, parent_b = self.find_parent(a), self.find_parent(b)
-        if parent_a == parent_b:
-            return
-        if self.weights[parent_a] > self.weights[parent_b]:
-            self.parents[parent_b] = parent_a
-            self.weights[parent_a] += self.weights[parent_b]
+        if self.weights[city_1_parent] > self.weights[city_2_parent]:
+            self.parents[city_2_parent] = city_1_parent
+            self.weights[city_1_parent] += self.weights[city_2_parent]
         else:
-            self.parents[parent_a] = parent_b
-            self.weights[parent_b] += self.weights[parent_a]    
+            self.parents[city_1_parent] = city_2_parent
+            self.weights[city_2_parent] += self.weights[city_1_parent]
     
-    def is_connected(self, a, b):
-        return self.find_parent(a) == self.find_parent(b)
+    def are_connected(self, city_1, city_2):
+        parent_city_1, parent_city_2 = self.find_parent(city_1), self.find_parent(city_2)
+        return parent_city_1 == parent_city_2
+        
+    def find_parent(self, city):
+        while self.parents[city] != city:
+            self.parents[city] = self.parents[self.parents[city]]
+            city = self.parents[city]
+        return city
     
-    def find_parent(self, a):
-        while a != self.parents[a]:
-            self.parents[a] = self.parents[self.parents[a]]
-            a = self.parents[a]
-        return a
-            
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        connections.sort(key = lambda a: a[2])
+        connections.sort(key = lambda connection: connection[2])
+        disjoint_set = DisjointSet(n)
         total_cost, total_connections = 0, 0
-        
-        dj = DisjointSet(n)
-        
-        for x, y, cost in connections:
-            if dj.is_connected(x, y):
+        for city_1, city_2, cost in connections:
+            if disjoint_set.are_connected(city_1, city_2):
                 continue
-            dj.union(x, y)
+            disjoint_set.union(city_1, city_2)
             total_cost += cost
             total_connections += 1
-
+        
         return total_cost if total_connections == n-1 else -1
+        
