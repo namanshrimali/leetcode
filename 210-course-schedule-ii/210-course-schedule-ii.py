481f9ca873f5e7c51f1a581ae7e22903b2a14545
Naman Shrimali <namanshrimali@gmail.com>
2022-06-11 17:00:24 -0400

    Time: 115 ms (70.26%), Space: 16 MB (50.27%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,30 +1,38 @@
+class CourseNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = set()
+        self.parents = set()
+    def does_not_have_prerequisites(self):
+        return len(self.parents) == 0
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        def is_node_parent(i):
-            return i not in rel_map or len(rel_map[i][0]) == 0
-
-        rel_map = {}
+        course_nodes = {}
+        scheduled_courses = []
+        no_prereq_left_queue = collections.deque([])
+        
         for course, prereq in prerequisites:
-            if course not in rel_map:
-                rel_map[course] = [set(), set()]
-            if prereq not in rel_map:
-                rel_map[prereq] = [set(), set()]
-            rel_map[course][0].add(prereq)
-            rel_map[prereq][1].add(course)
-        
-        deque = collections.deque([])
-        answer = []
-        
-        for i in range(numCourses):
-            if is_node_parent(i):
-                deque.append(i)
-        while deque:
-            node = deque.popleft()
-            answer.append(node)
-            for children in rel_map.get(node, [[], []])[1]:
-                rel_map[children][0].remove(node)
-                if is_node_parent(children):
-                    deque.append(children)
-        return [] if len(answer) != numCourses else answer
+            if course not in course_nodes:
+                course_nodes[course] = CourseNode(course)
+            if prereq not in course_nodes:
+                course_nodes[prereq] = CourseNode(prereq)
+            course_nodes[course].parents.add(course_nodes[prereq])
+            course_nodes[prereq].children.add(course_nodes[course])
             
         
+        for course in range(numCourses):
+            if course not in course_nodes or course_nodes[course].does_not_have_prerequisites():
+                no_prereq_left_queue.append(course_nodes.get(course, CourseNode(course)))
+        
+        
+        while no_prereq_left_queue:
+            parent_course = no_prereq_left_queue.popleft()
+            scheduled_courses.append(parent_course.value)
+            for child_course in parent_course.children:
+                child_course.parents.remove(parent_course)
+                if child_course.does_not_have_prerequisites():
+                    no_prereq_left_queue.append(child_course)
+        return scheduled_courses if len(scheduled_courses) == numCourses else []
+            
+                
