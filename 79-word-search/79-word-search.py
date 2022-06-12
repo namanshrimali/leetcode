8b89815cd30ed7d5c167e4a198933df28d27bd84
Naman Shrimali <namanshrimali@gmail.com>
2022-06-12 10:45:58 -0400

    Time: 6548 ms (59.34%), Space: 14 MB (49.97%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,23 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
         directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-        m,  n = len(board), len(board[0])
         
-        def find_word(row, col, suffix):
-            if len(suffix) == 0:
+        def coordinates_are_valid(x, y):
+            return -1 < x < m and -1 < y < n
+        
+        def find_word(x, y, suffix):
+            if len(suffix)  == 0:
                 return True
-            if not (-1 < row < m and -1 < col < n and board[row][col] == suffix[0]):
+            if not coordinates_are_valid(x, y) or board[x][y] != suffix[0]:
                 return False
-            is_found = False
-            board[row][col] = '#'
-            
+            if_found = False
+            board[x][y] = '#'
             for x_dir, y_dir in directions:
-                is_found = find_word(row+x_dir, col+y_dir, suffix[1:])
-                
+                is_found = find_word(x+x_dir, y+y_dir, suffix[1:])
                 if is_found:
                     break
-            board[row][col] = suffix[0]
+            board[x][y] = suffix[0]
             return is_found
+                
             
         for i in range(m):
             for j in range(n):
