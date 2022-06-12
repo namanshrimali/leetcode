8cbf5889178667018198883cd45dd8ffda68ed97
Naman Shrimali <namanshrimali@gmail.com>
2022-06-12 09:34:30 -0400

    Time: 453 ms (30.54%), Space: 18.2 MB (49.32%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,14 +1,16 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        remainder_counter = {}
         total_pairs = 0
-        found_map = {}
         for t in time:
-            t_mod_60 = t%60
-            if t_mod_60 == 0:
-                total_pairs += found_map.get(0, 0)
-            else:
-                complement = 60-t_mod_60
-                if complement in found_map:
-                    total_pairs += found_map[complement]
-            found_map[t_mod_60] = found_map.get(t_mod_60, 0) + 1
+            remainder = t % 60
+            complement = 60 - remainder
+            
+            if remainder == 0 and 0 in remainder_counter:
+                total_pairs += remainder_counter[remainder]
+            elif complement in remainder_counter:
+                total_pairs += remainder_counter[complement]
+            
+            
+            remainder_counter[remainder] = remainder_counter.get(remainder, 0) + 1
         return total_pairs
