6e96785142aacd90ed491809071b36916d774642
Naman Shrimali <namanshrimali@gmail.com>
2022-06-14 08:46:45 -0400

    Time: 70 ms (25.89%), Space: 14.3 MB (8.28%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,23 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordDict = set(wordDict)
-        L = len(s)
-        word_idx_status = [None] * L
-        is_found = False
+        n = len(s)
+        found_indices = [None] * n
         def backtrack(curr_idx = 0):
             nonlocal is_found
             if is_found:
                 return
-            if curr_idx == L:
+            if curr_idx == n:
                 is_found = True
                 return
-            if word_idx_status[curr_idx] == False:
+            if found_indices[curr_idx] == False:
                 return
-            for next_idx in range(curr_idx+1, L+1):
+            for next_idx in range(curr_idx + 1, n + 1):
                 if s[curr_idx:next_idx] in wordDict:
                     backtrack(next_idx)
                 if is_found:
                     break
-            word_idx_status[curr_idx] = is_found
+            found_indices[curr_idx] = is_found
+        is_found = False
         backtrack()
-        return is_found 
+        return is_found
