372d335d1f628a5179b9512884f8c29eedcd1bdc
Naman Shrimali <namanshrimali@gmail.com>
2022-06-15 23:05:45 -0400

    Time: 382 ms (17.55%), Space: 14.2 MB (96.04%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,42 +1,41 @@
 class DisjointSet:
-    def __init__(self, n):
-        self.parents = [i for i in range(n)]
-        self.weights = [1]*n
+    def __init__(self, total_points):
+        self.parents = [i for i in range(total_points)]
+        self.weights = [1] * total_points
+        self.total_points = total_points
     
-    def union(self, a, b):
-        parent_a, parent_b = self.get_parent(a), self.get_parent(b)
+    def union(self, city_a, city_b):
+        parent_a, parent_b = self.find_parent(city_a), self.find_parent(city_b)
         if parent_a == parent_b:
             return
-        if self.weights[parent_b] > self.weights[parent_a]:
-            self.parents[parent_a] = parent_b
-            self.weights[parent_b] += self.weights[parent_a]
-        else:
+        if self.weights[parent_a] > self.weights[parent_b]:
             self.parents[parent_b] = parent_a
             self.weights[parent_a] += self.weights[parent_b]
+        else:
+            self.parents[parent_a] = parent_b
+            self.weights[parent_b] += self.weights[parent_a]
     
+    def find_parent(self, city):
+        while self.parents[city] != city:
+            self.parents[city] = self.parents[self.parents[city]]
+            city = self.parents[city]
+        return city
     
-    def are_connected(self, a, b):
-        parent_a, parent_b = self.get_parent(a), self.get_parent(b)
-        return parent_a == parent_b
-    
-        
-    def get_parent(self, a):
-        while a != self.parents[a]:
-            self.parents[a] = self.parents[self.parents[a]]
-            a = self.parents[a]
-        return a
+    def are_connected(self, city_a, city_b):
+        return self.find_parent(city_a) == self.find_parent(city_b)
     
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        dset = DisjointSet(n)
+        province_set = DisjointSet(n)
         
+        for city_a in range(n):
+            for city_b in range(city_a + 1, n):
+                if isConnected[city_a][city_b] == 1:
+                    province_set.union(city_a, city_b)
+    
+        unique_provinces = set()
         for i in range(n):
-            for j in range(n):
-                if isConnected[i][j] == 1:
-                    dset.union(i, j)
-        parents = set()
-        for i in range(n):
-            parents.add(dset.get_parent(i))
-        return len(parents)
+            unique_provinces.add(province_set.find_parent(i))
         
+        return len(unique_provinces)
