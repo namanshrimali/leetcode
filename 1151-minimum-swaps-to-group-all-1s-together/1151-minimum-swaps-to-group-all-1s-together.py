255efda852f3851003ac731ff622989bf76cb222
Naman Shrimali <namanshrimali@gmail.com>
2022-06-16 18:25:11 -0400

    Time: 946 ms (68.98%), Space: 18.1 MB (15.02%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -1,22 +1,12 @@
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
-        window_size = 0
+        window_size = sum(data) # total ones is the window size
+        total_swaps_required = zeros_in_window = window_size - sum(data[:window_size])
         
-        for num in data:
-            window_size += num
-        
-        zeros_in_window = 0
-        
-        for i in range(window_size):
-            if data[i] == 0:
-                zeros_in_window += 1
-        min_swaps = zeros_in_window  
-
         for i in range(1, len(data) - window_size + 1):
             if data[i-1] == 0:
                 zeros_in_window -= 1
-            if data[i+window_size - 1] == 0:
+            if data[i + window_size - 1] == 0:
                 zeros_in_window += 1
-
-            min_swaps = min(min_swaps, zeros_in_window)
-        return min_swaps
+            total_swaps_required = min(total_swaps_required, zeros_in_window)
+        return total_swaps_required
