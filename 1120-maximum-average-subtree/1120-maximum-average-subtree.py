7a950658ebb86bf06103594bbbb28f259d3f3974
Naman Shrimali <namanshrimali@gmail.com>
2022-06-16 21:16:02 -0400

    Time: 62 ms (66.67%), Space: 17 MB (42.59%) - LeetHub

--- 1120-maximum-average-subtree/1120-maximum-average-subtree.py
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.py
@@ -6,22 +6,18 @@
 #         self.right = right
 class Solution:
     def maximumAverageSubtree(self, root: Optional[TreeNode]) -> float:
-        def dfs(node):
+        max_avg = 0
+        def subtree_average_calculator(node):
             nonlocal max_avg
             if node is None:
                 return 0, 0
-            total_subtree_size, subtree_total = 0, 0
+            left_subtree_len, left_subtree_total = subtree_average_calculator(node.left)
+            right_subtree_len, right_subtree_total = subtree_average_calculator(node.right)
             
-            left_subtree_size, left_total = dfs(node.left)
-            right_subtree_size, right_total = dfs(node.right)
-            
-            total_subtree_size = left_subtree_size + right_subtree_size + 1
-            total_subtree_val = left_total + right_total + node.val
-            
-            max_avg = max(max_avg, total_subtree_val/total_subtree_size)
-            
-            return total_subtree_size, total_subtree_val
-        max_avg = 0
-        dfs(root)
+            curr_tree_total = left_subtree_total + right_subtree_total + node.val
+            curr_tree_len = left_subtree_len + right_subtree_len + 1
+            max_avg = max(max_avg, curr_tree_total/curr_tree_len)
+            return curr_tree_len, curr_tree_total
+        subtree_average_calculator(root)
         return max_avg
         
