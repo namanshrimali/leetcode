10adb578583f2145523674b097043abacc5f1848
Naman Shrimali <namanshrimali@gmail.com>
2022-06-16 21:30:53 -0400

    Time: 735 ms (30.94%), Space: 61.6 MB (21.14%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,28 +1,30 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.dict = {}
-        self.arr = []
-        
+        self.item_idx_map = {}
+        self.item_array = []
 
     def insert(self, val: int) -> bool:
-        if val in self.dict:
+        if val in self.item_idx_map:
             return False
-        self.dict[val] = len(self.arr)
-        self.arr.append(val)
+        self.item_idx_map[val] = len(self.item_array)
+        self.item_array.append(val)
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.dict:
+        if val not in self.item_idx_map:
             return False
-        val_idx = self.dict[val]
-        self.arr[-1], self.arr[val_idx] = self.arr[val_idx], self.arr[-1]
-        self.dict[self.arr[val_idx]] = val_idx
-        del self.dict[self.arr.pop()]
+        val_idx = self.item_idx_map[val]
+        self.item_array[val_idx], self.item_array[-1] = self.item_array[-1], self.item_array[val_idx]
+        last_val = self.item_array[val_idx]
+        self.item_idx_map[last_val] = val_idx
+        self.item_array.pop()
+        del self.item_idx_map[val]
         return True
 
     def getRandom(self) -> int:
-        return self.arr[random.randint(0, len(self.arr)-1)]
+        return self.item_array[random.randint(0, len(self.item_array)-1)]
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
