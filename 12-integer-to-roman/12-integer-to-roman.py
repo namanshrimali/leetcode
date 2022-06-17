a4d5b5a453734b159bff663dda7b88d0335117ad
Naman Shrimali <namanshrimali@gmail.com>
2022-06-16 21:46:15 -0400

    Time: 134 ms (5.08%), Space: 13.9 MB (34.98%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,33 +1,25 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman = []
-        values = [1000, 500, 100, 50, 10, 5, 1]
-        symbol_mapping = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V', 1: 'I'}
-        special_mapping = {
-            'M': 100,
-            'D': 100,
-            'C': 10,
-            'L': 10,
-            'V': 1,
-            'X': 1
+        int_roman_dict = {
+            1 : 'I',
+            4 : 'IV',
+            5 : 'V',
+            9 : 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000:'M'
         }
-            
-            
-        value_idx = 0
-        while num:
-            curr_value = values[value_idx]
-            curr_symbol = symbol_mapping[curr_value]
-            roman.append((num//curr_value)*curr_symbol)
-            num = num%values[value_idx]
-            
-            if curr_symbol in special_mapping:
-            
-                one_lower = curr_value - special_mapping[curr_symbol]
-
-                if num // one_lower > 0:
-                    num = num % one_lower
-                    lower_symbol = symbol_mapping[special_mapping[curr_symbol]]
-                    roman.append(lower_symbol + curr_symbol)
-
-            value_idx +=1
-        return ''.join(roman)
+        dividends = list(sorted(int_roman_dict.keys(), reverse = True))
+        i = 0
+        roman_rep = []
+        while num and i < len(dividends):
+            roman_rep.append((num//dividends[i]) * int_roman_dict[dividends[i]])
+            num = num % dividends[i] 
+            i += 1
+        return ''.join(roman_rep)
