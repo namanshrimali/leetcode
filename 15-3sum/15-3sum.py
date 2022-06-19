15c98ce90f4e60eb4030bb8067ee6702a2301364
Naman Shrimali <namanshrimali@gmail.com>
2022-06-19 12:03:01 -0400

    Time: 1018 ms (66.66%), Space: 19.1 MB (8.62%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,25 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
         nums.sort()
-        answer = []
-        
-        def two_sum(j, first_num):
-            seen = set()
+        n = len(nums)
+        result = []
+        def two_sum(j, target):
+            seen_set = set()
             while j < n:
-                complement = -first_num - nums[j]
-                if complement in seen:
-                    answer.append([first_num, complement, nums[j]])
-                    while j+1 < n and nums[j] == nums[j++1]:
+                complement = target - nums[j]
+                if complement in seen_set:
+                    result.append([-target, complement, nums[j]])
+                    while j+1 < n and nums[j] == nums[j+1]:
                         j+=1
-                seen.add(nums[j])
+                seen_set.add(nums[j])
+                
                 j+=1
-
-        for i in range(n):
-            if nums[i]>0:
+    
+        i = 0
+        while i < n:
+            if nums[i] > 0:
                 break
-            if i == 0 or nums[i-1] != nums[i]:
-                two_sum(i+1, nums[i])
+            if i < 1 or nums[i-1] != nums[i]:
+                two_sum(i+1, -nums[i])
+
+            i += 1
+        return result
             
-        return answer
-        
