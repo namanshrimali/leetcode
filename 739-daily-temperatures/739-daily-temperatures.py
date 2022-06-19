eeede490895f8d5af113c707cd994c40198df128
Naman Shrimali <namanshrimali@gmail.com>
2022-06-19 19:12:24 -0400

    Time: 1200 ms (96.95%), Space: 25 MB (49.44%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -3,12 +3,14 @@ class Solution:
         n = len(temperatures)
         answer = [0] * n
         max_temperature = 0
-        for curr_day in range(n-1, -1, -1):
-            if max_temperature <= temperatures[curr_day]:
-                max_temperature = temperatures[curr_day]
+        for curr_idx in range(n-1, -1, -1):
+            curr_temperature = temperatures[curr_idx]
+            if curr_temperature >= max_temperature:
+                max_temperature = curr_temperature
                 continue
             days = 1
-            while temperatures[curr_day + days] <= temperatures[curr_day]:
-                days += answer[curr_day + days]
-            answer[curr_day] = days
+            while temperatures[curr_idx + days] <= temperatures[curr_idx]:
+                days += answer[curr_idx + days]
+            answer[curr_idx] = days
         return answer
+        
