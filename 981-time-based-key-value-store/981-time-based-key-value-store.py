d155203b7da92633748e29824039982a12b41dd1
Naman Shrimali <namanshrimali@gmail.com>
2022-06-23 17:44:06 -0400

    Time: 1367 ms (16.50%), Space: 68.6 MB (93.47%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,38 +1,38 @@
-class Key:
+class KeyNode:
     def __init__(self):
-        self.values = []
         self.timestamps = []
-        
+        self.timestamp_values = []
+    
     def get_value_for_timestamp(self, timestamp):
-        left, right = 0, len(self.values)-1
-        while left <= right:
-            mid = (left+right)//2
-            if self.timestamps[mid] > timestamp:
-                right = mid - 1
+        left, right = 0, len(self.timestamps) - 1
+        while left < right:
+            mid = math.ceil((left+right)/2)
+            if self.timestamps[mid] <= timestamp:
+                left = mid
             else:
-                left = mid + 1
-        if self.timestamps[right] > timestamp:
-            return ''
-        return self.values[right]
+                right = mid - 1
+        return '' if self.timestamps[left] > timestamp else self.timestamp_values[left]
     
-    def add_value_and_timestamp(self, value, timestamp):
-        self.values.append(value)
+    def set_timestamp_value(self, timestamp, value):
         self.timestamps.append(timestamp)
-    
+        self.timestamp_values.append(value)
+        
 class TimeMap:
 
     def __init__(self):
-        self.keys = {}
+        self.key_timestamp_value_map = {}
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.keys:
-            self.keys[key] = Key()
-        self.keys[key].add_value_and_timestamp(value, timestamp)
+        self.key_timestamp_value_map[key] = self.key_timestamp_value_map.get(key, KeyNode())
+        key_node = self.key_timestamp_value_map[key]
+        key_node.set_timestamp_value(timestamp, value)
+        
 
     def get(self, key: str, timestamp: int) -> str:
-        if key in self.keys:
-            return self.keys[key].get_value_for_timestamp(timestamp)
-        return ''
+        if key not in self.key_timestamp_value_map:
+            return ""
+        return self.key_timestamp_value_map[key].get_value_for_timestamp(timestamp)
         
 
 
