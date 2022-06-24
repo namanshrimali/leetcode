d95b6c5ca2cd26f0cb2f03f23f61f1576dfdba9f
Naman Shrimali <namanshrimali@gmail.com>
2022-06-23 22:34:29 -0400

    Time: 46 ms (86.33%), Space: 14.2 MB (56.45%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,45 +1,37 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # target == pivot
-        # target on left -> > pivot and < nums[0]
-        # target on right -> > pivot and > nums[0]
-        
-        # first task -> find pivot
-        # second task -> check whether target is on left or on right of pivot
-        # find target on whichever side it is in
-        
         def find_pivot():
             left, right = 0, len(nums)-1
-            # pivot is element who is lesser than elements on left (if present) and elements on right (if present)
             while left < right:
                 mid = (left+right)//2
                 if nums[mid] >= nums[0] and nums[mid] > nums[-1]:
                     left = mid + 1
-                else:   # nums[mid] <  maybe on pivot or maybe on right of pivot
-                    # check if mid is pivot
-                    if (mid - 1 == -1 or nums[mid-1] > nums[mid]) and (mid + 1 == len(nums) or nums[mid+1] > nums[mid]):
+                else:
+                    if (mid == 0 or nums[mid-1]> nums[mid]) and (mid == len(nums)-1 or nums[mid+1]>nums[mid]):
                         return mid
-                    # if not pivot, then on right of pivot
-                    right = mid - 1
+                    else:
+                        right = mid - 1
             return left
+        
         
         def binary_search(left, right):
             while left <= right:
-                mid = (left+right)//2
+                mid = (left + right)//2
                 if nums[mid] == target:
                     return mid
                 elif nums[mid] > target:
-                    right = mid-1
+                    right = mid - 1
                 else:
                     left = mid + 1
             return -1
-            
         
         pivot_idx = find_pivot()
-
+        print(pivot_idx)
+        
         if target == nums[pivot_idx]:
             return pivot_idx
         elif target > nums[pivot_idx] and target <= nums[-1]:
             return binary_search(pivot_idx, len(nums)-1)
         else:
-            return binary_search(0, pivot_idx-1)
+            
+            return binary_search(0, pivot_idx - 1)
