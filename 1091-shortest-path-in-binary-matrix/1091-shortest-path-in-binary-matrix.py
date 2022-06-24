ff621025f1b8e915bf977d4f286d9145a6b98424
Naman Shrimali <namanshrimali@gmail.com>
2022-06-24 19:06:16 -0400

    Time: 857 ms (59.07%), Space: 14.2 MB (95.14%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,30 +1,28 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, -1), (1, -1), (-1, 1), (1, 1)]
-        n = len(grid)
-        if grid[0][0] != 0 or grid[n-1][n-1] != 0:
-            return -1
-        deque = collections.deque([(0, 0)])
-        grid[0][0] = -1
-
-        while deque:
-            curr_x, curr_y = deque.popleft()
-            
-            if grid[curr_x][curr_y] == '*':
-                continue
-            
-            curr_path = -grid[curr_x][curr_y]
-            
-            if (curr_x, curr_y) == (n-1, n-1):
-                return curr_path
-
-            for x, y in directions:
-                next_x, next_y = curr_x + x, curr_y + y
-                if -1 < next_x < n and -1 < next_y < n and grid[next_x][next_y] == 0:
-                    deque.append((next_x, next_y))
-                    grid[next_x][next_y] = - curr_path - 1
-
-            grid[curr_x][curr_y] = '*'
-            
-        return -1
+        m, n = len(grid), len(grid[0])
         
+        if grid[0][0] != 0 or grid[m-1][n-1] != 0:
+            return -1
+        
+        def are_coordinates_valid(x, y):
+            return -1 < x < m and -1 < y < n and grid[x][y] != 1
+        
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        
+        curr_path_len = 0
+        deque = collections.deque([(0, 0)])
+        grid[0][0] = 1
+        while deque:
+            curr_path_len += 1
+            deque_len = len(deque)
+            for _ in range(deque_len):
+                curr_x, curr_y = deque.popleft()
+                if (curr_x, curr_y) == (m-1, n-1):
+                    return curr_path_len
+                for x_dir, y_dir in directions:
+                    next_x, next_y = curr_x + x_dir, curr_y + y_dir
+                    if are_coordinates_valid(next_x, next_y):
+                        grid[next_x][next_y] = 1
+                        deque.append((next_x, next_y))
+        return -1
