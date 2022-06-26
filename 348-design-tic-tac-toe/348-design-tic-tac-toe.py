25e7106bb09cb7e8b9cbecb3c0866c7e35a977e2
Naman Shrimali <namanshrimali@gmail.com>
2022-06-26 18:57:31 -0400

    Time: 116 ms (64.42%), Space: 16.6 MB (98.04%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -1,25 +1,29 @@
 class TicTacToe:
 
     def __init__(self, n: int):
-        self.row_sum = [0] * n
-        self.col_sum = [0] * n
-        self.diag_sum = 0
-        self.anti_diag_sum = 0
         self.n = n
-
+        self.row_sums = [0] * n
+        self.col_sums = [0] * n
+        self.diagonal_sum = 0
+        self.anti_diagonal_sum = 0
+            
     def move(self, row: int, col: int, player: int) -> int:
-        player_offset = 1 if player == 1 else -1
-        n = self.n
-        self.row_sum[row] += player_offset
-        self.col_sum[col] += player_offset
+        player_id = player
+        player = 1 if player == 1 else -1
+        self.row_sums[row] += player
+        self.col_sums[col] += player
         if row == col:
-            self.diag_sum += player_offset
-        if row == n - col - 1:
-            self.anti_diag_sum += player_offset
+            self.diagonal_sum += player
+        if row == self.n - col - 1:
+            self.anti_diagonal_sum += player
         
-        if self.row_sum[row] in (n, -n) or self.col_sum[col] in (n, -n) or self.diag_sum in (n, -n) or self.anti_diag_sum in (n, -n):
-            return player
+        mult_factor = player
+        
+        if self.row_sums[row] * mult_factor == self.n or self.col_sums[col] * mult_factor == self.n or self.anti_diagonal_sum * mult_factor == self.n or self.diagonal_sum * mult_factor == self.n:
+            return player_id
         return 0
+        
+        
 
 
 # Your TicTacToe object will be instantiated and called as such:
