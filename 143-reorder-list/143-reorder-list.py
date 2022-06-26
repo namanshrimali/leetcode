d8ec5c81973407bbd2ad97ce6cf74362828e0f44
Naman Shrimali <namanshrimali@gmail.com>
2022-06-26 16:01:28 -0400

    Time: 167 ms (18.99%), Space: 24.1 MB (28.14%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -12,19 +12,13 @@ class Solution:
             return head
         
         def find_list_mid(list_head):
-            list_size = 0
-            curr_node = list_head
-            while curr_node is not None:
-                curr_node = curr_node.next
-                list_size += 1
-            list_mid = int(list_size/2)
-            mid_head = list_head
-            
-            for _ in range(list_mid-1):
-                mid_head = mid_head.next
-            mid_node = mid_head.next
-            mid_head.next = None
-            return mid_node
+            prev, tortoise, hare = list_head, list_head, list_head
+            while hare and hare.next:
+                prev = tortoise
+                tortoise = tortoise.next
+                hare = hare.next.next
+            prev.next = None
+            return tortoise
         
         def reverse_list(list_head):
             curr_node, prev_node = list_head, None
@@ -36,20 +30,19 @@ class Solution:
             return prev_node
                 
         def merge_lists(list1_head, list2_head):
-            if list1_head is None or list2_head is None:
-                return
-            prev = ListNode()
-            
+            dummy_head = prev_node = ListNode()
             while list1_head and list2_head:
-                list1_next, list2_next = list1_head.next, list2_head.next
-                prev.next = list1_head
+                next_list1_head, next_list2_head = list1_head.next, list2_head.next
+                prev_node.next = list1_head
                 list1_head.next = list2_head
-                prev = list2_head
-                list1_head, list2_head = list1_next, list2_next
+                prev_node = list2_head
+                list1_head, list2_head = next_list1_head, next_list2_head
             if list2_head:
-                prev.next = list2_head
+                prev_node.next = list2_head
                 list2_head.next = None
                 
+                
+            
         list1_head, list2_head = head, find_list_mid(head)
         list2_head = reverse_list(list2_head)
         merge_lists(list1_head, list2_head)
