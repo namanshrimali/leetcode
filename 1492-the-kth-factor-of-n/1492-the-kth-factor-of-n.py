ce8c2a70b98864d2254abce14cd7bab8beb02fae
Naman Shrimali <namanshrimali@gmail.com>
2022-06-26 18:53:09 -0400

    Time: 39 ms (75.20%), Space: 13.9 MB (15.21%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -3,13 +3,14 @@ class Solution:
         sqrt_n = int(n**(0.5))
         is_perfect_square = sqrt_n ** 2 == n
         factors = []
+        factor_len = 0
         for i in range(1, sqrt_n+1):
             if n%i == 0:
                 factors.append(i)
                 k -= 1
+                factor_len += 1
             if k == 0:
                 return factors[-1]
         if is_perfect_square:
             k+=1
-        
-        return n//factors[len(factors) - k] if k <= len(factors) else -1
+        return n//factors[factor_len - k] if k <= factor_len else -1
