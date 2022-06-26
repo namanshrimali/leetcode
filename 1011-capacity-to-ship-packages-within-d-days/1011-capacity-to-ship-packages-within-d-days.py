8bbe3094abd689779fdce5d7a2e7e58b163593a4
Naman Shrimali <namanshrimali@gmail.com>
2022-06-26 16:37:03 -0400

    Time: 622 ms (67.81%), Space: 17.4 MB (8.62%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,24 +1,31 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        n = len(weights)
-        def can_fit_within_days(capacity):
-            total, days_spent, i = 0, 0, 0
-            while i < n and days_spent < days:
-                total += weights[i]
-                
-                if total > capacity:
-                    if days_spent == days - 1:
-                        break
-                    days_spent += 1
-                    total = weights[i]
-                i+=1
-            return i == n
-        start, end = max(weights), sum(weights)
-        while start < end:
-            mid = (start + end) // 2
-            if can_fit_within_days(mid):
-                end = mid
-            else:
-                start = mid + 1
-        return end
+        
+        def can_fit_with(given_capacity):
+            curr_day, curr_capacity = 0, 0
             
+            for i in range(len(weights)):
+                curr_capacity += weights[i]
+                if curr_capacity > given_capacity:
+                    if curr_day == days - 1:
+                        return False
+                    curr_capacity = weights[i]
+                    curr_day += 1
+            return True
+        
+        def binary_search(start, end):
+            while start < end:
+                mid = (start+end)//2
+                if can_fit_with(mid):
+                    end = mid
+                else:
+                    start = mid+1
+            return end
+        
+        min_capacity, max_capacity = 0, 0
+        for weight in weights:
+            min_capacity = max(min_capacity, weight)
+            max_capacity += weight
+        
+        return binary_search(min_capacity, max_capacity)
+        
