a900fd5e6de75f29d475608d67bcce61ccdbae3b
Naman Shrimali <namanshrimali@gmail.com>
2022-06-28 21:59:27 -0400

    Time: 57 ms (30.70%), Space: 14 MB (5.88%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,31 +1,58 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        might_be_pending = len(moves) < 9
-        curr_player = 1 # A is 1, B is -1
-        winner = None
-        row_total = [0]*3
-        col_total = [0]*3
-        diag_total = 0
-        anti_diag_total = 0
+        n = 3
+        total_players = 2
+        are_moves_possible = len(moves) < n**2
+        curr_player = 0
+        move_sum_per_row = [0] * n
+        move_sum_per_col = [0] * n
+        anti_diag_sum = 0
+        diag_sum = 0
+        has_won = False
+        
+        def is_diagonal(row, col):
+            return row == col
+        def is_anti_diagonal(row, col):
+            return row == n - col - 1
+        def has_row_crossed(row):
+            return move_sum_per_row[row] in (n, -n)
+        def has_column_crossed(col):
+            return move_sum_per_col[col] in (n, -n)
+        
+        def has_diag_crossed_for_player(player):
+            if player == 0:
+                return diag_sum == n
+            return diag_sum == -n
+        
+        def has_anti_diag_crossed_for_player(player):
+            if player == 0:
+                return anti_diag_sum == n
+            return anti_diag_sum == -n
+        
+        def get_move(player):
+            return 1 if player == 0 else -1
         
         for row, col in moves:
-            col_total[col] += curr_player
-            row_total[row] += curr_player
-            if row == col:
-                diag_total += curr_player
-            if row == 3-col-1:
-                anti_diag_total += curr_player
+            move = get_move(curr_player)
+            move_sum_per_row[row] += move
+            move_sum_per_col[col] += move
             
-            if row_total[row] in (-3, 3) or col_total[col] in (-3, 3) or diag_total in (-3, 3) or anti_diag_total in (-3, 3):
-                winner = curr_player
+            if is_diagonal(row, col):
+                diag_sum+=move
+            
+            if is_anti_diagonal(row, col):
+                anti_diag_sum += move
+            
+            if has_row_crossed(row) or has_column_crossed(col) or has_diag_crossed_for_player(curr_player) or has_anti_diag_crossed_for_player(curr_player):
+                has_won = True
                 break
-                
-            curr_player = - curr_player
-        
-        if winner:
-            return 'A' if winner == 1 else 'B'
-        elif might_be_pending:
-            return 'Pending'
-        else:
-            return 'Draw'
             
+            curr_player = (curr_player + 1)%total_players
+        
+        if has_won:
+            return "A" if curr_player == 0 else "B"
+        elif are_moves_possible:
+            return "Pending"
+        else:
+            return "Draw"
+        
