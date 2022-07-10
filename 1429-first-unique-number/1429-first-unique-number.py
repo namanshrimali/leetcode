7c74bad57bc127a2190fc6fb237668ef50a59c5f
Naman Shrimali <namanshrimali@gmail.com>
2022-07-09 20:52:11 -0400

    Time: 782 ms (94.74%), Space: 58.5 MB (39.85%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.py
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -1,34 +1,22 @@
 class FirstUnique:
 
     def __init__(self, nums: List[int]):
-        self.num_counter = {}
-        self._count_numbers(nums)
-        self.deque = collections.deque([])
-        self._fill_nums(nums)
-    
+        self.queue = collections.deque(nums)
+        self.counter = collections.defaultdict(int)
+        for num in self.queue:
+            self.counter[num]+=1
 
     def showFirstUnique(self) -> int:
-        while self.deque and self.num_counter[self.deque[0]] > 1:
-            self.deque.popleft()
-        if not self.deque:
-            return -1
-        return self.deque[0]
+        while self.queue:
+            top_ele = self.queue[0]
+            if self.counter[top_ele] == 1:
+                return top_ele
+            self.queue.popleft()
+        return -1
 
     def add(self, value: int) -> None:
-        self.num_counter[value] = self.num_counter.get(value, 0) + 1
-        if self.num_counter[value] == 1:
-            self.deque.append(value)
-        
-    def _count_numbers(self, nums):
-        for num in nums:
-            self.num_counter[num] = self.num_counter.get(num, 0) + 1
-    def _fill_nums(self, nums):
-        for num in nums:
-            if self.num_counter[num] > 1:
-                continue
-            self.deque.append(num)
-        
-        
+        self.queue.append(value)
+        self.counter[value]+=1
 
 
 # Your FirstUnique object will be instantiated and called as such:
